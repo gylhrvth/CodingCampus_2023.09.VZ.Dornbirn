ea9e786e2d299cfa1e9873684f00ac05b816ee8e
Michael Hämmerle <khaemmerle2@gmx.at>
2023-09-26 15:57:38 +0200

    day 1 exercises

--- Michael/Week01/HelloWorld.cs
+++ Michael/Week01/HelloWorld.cs
@@ -5,8 +5,61 @@ namespace Michael.Week01
 	{
 		public static void Start()
 		{
-			Console.WriteLine("Hello MWorld!");
-		}
-	}
-}
+			Console.WriteLine("Hello World!");
+            Console.WriteLine(" ___    ___");
+            Console.WriteLine("|   \\  /   |");
+            Console.WriteLine("| |\\ \\/ /| |");
+            Console.WriteLine("| | \\__/ | |");
+            Console.WriteLine("| |      | |");
+            Console.WriteLine("| |      | |");
+            Console.WriteLine("|_|      |_|");
 
+            Console.WriteLine("    ")
+            Console.WriteLine("     |-|	");
+            Console.WriteLine("     | |	");
+            Console.WriteLine("     | |	");
+            Console.WriteLine("     | |	");
+            Console.WriteLine("     | |	");
+            Console.WriteLine("     |_|	");
+
+            Console.WriteLine("   -------\n  |  _____|\n  | |\n  | |\n  | |_____\n  |_______|");
+
+            Console.WriteLine(@"     /--\
+    / /\ \
+   / /  \ \
+  / /    \ \
+ /  ======  \
+/_/        \_\");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int counter = 0;
+
+            while (counter <= 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            counter = 0;
+
+            while (counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+
+
+
+        }
+}
+}
