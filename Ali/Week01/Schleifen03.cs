4c783c9f00481a86ab5db28255f3b229a7802f44
AliAsuk <asuk_ali@icloud.com>
2023-09-27 11:48:21 +0200

    Schleifen

---
+++ Ali/Week01/Schleifen03.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Ali.Week01
+{
+    public class Schleifen03
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Hello World1");
+
+            
+            for (int i = 0; i < 11; ++i)
+            {
+                Console.WriteLine(i);
+            }
+            int j = 0;
+            while (j < 11)
+            {
+                Console.WriteLine(j);
+                j++;
+              
+            }
+            for (int k = 0; k < 10; ++k)
+            {
+                Console.WriteLine(k);
+            }
+             
+            for (int x = 0; x<= 10; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine("ist gerade " + x);
+                }
+                
+            }
+            
+           
+            
+                
+            
+
+             
+                   
+        }
+    }
+}
