9d077c891282cffeb4c29dc15efbc52330978935
Riccardo Ceccon <riccardo.ceccon@blum.com>
2023-09-27 12:17:22 +0200

    .

--- Dimitri/Week01/HelloWorld.cs
+++ Dimitri/Week01/HelloWorld.cs
@@ -8,7 +8,7 @@ namespace Dimitri.Week01
 		public static void Start()
 		{
             Console.OutputEncoding = Encoding.UTF8; //set console output to utf8 encoding 
-            Console.WriteLine("Hello Dimitri!")
+            Console.WriteLine("Hello Dimitri!");
 			Console.WriteLine("Hello World!\r\n\r\n     /--\\\r\n    / /\\ \\\r\n   / /  \\ \\\r\n  / /    \\ \\\r\n /  ======  \\\r\n/_/        \\_\\");
 			Console.WriteLine("     /--\\");
 			Console.WriteLine("    / /\\ \\");
