6c13ce4de0909c78a52892b3feb43727a12fbe6b
Cemo38 <cemalkosar38@gmail.com>
2023-10-03 10:26:22 +0200

    slash

--- Cemal/Week02/BooleanExamples.cs
+++ Cemal/Week02/BooleanExamples.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,48 @@ namespace Cemal.Week02
     {
         public static void StartBooleanExamples()
         {
-            bool 
+            Console.WriteLine("Aufgabe Print Slash");
+            PrintSlash("x", 3, true);
+
+            Console.WriteLine("Aufgabe Print Slash");
+            PrintPyramid("x", 5);
+        }
+
+        public static void PrintSlash(String symbol, int count, bool cas)
+        {
+            if (cas == true)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    for (int y = 0; y < i; y++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(symbol);
+                    Console.WriteLine();
+                }
+            }
+
+            if (cas == false)
+            {
+                count = 4;
+                symbol = "y";
+
+                for (int i = 0; i < count; i++)
+                {
+                    for (int y = i; y < count; y++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(symbol);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static void PrintPyramid(String symbol, int count)
+        {
+
         }
     }
 }
