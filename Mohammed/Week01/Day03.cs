9fa63c7cccb1f0cb6799834f26ce0fd3a61f0f7e
Mohammed <alsarajimohammed@gmail.com>
2023-10-03 10:34:05 +0200

    Test

---
+++ Mohammed/Week01/Day03.cs
@@ -0,0 +1,312 @@
+﻿using System;
+namespace Mohammed.Week01
+{
+    public class Day03
+    {
+        private static int i;
+
+        public Day03()
+        {
+        }
+
+        public static void Start()
+        {
+            //printChars
+            Console.WriteLine("Writing line of characters:");
+            printChars("x", 10);
+            Console.WriteLine();
+
+            //printSquare
+            Console.WriteLine("Writing line of Square:");
+            printSquare("n", 10);
+            Console.WriteLine();
+
+            //printRect
+            Console.WriteLine("Writing line of Rectangle:");
+            printRect("i", 10, 3);
+            Console.WriteLine();
+
+            //PrintTriangleBottomLeft
+            Console.WriteLine("Writing some line of trianleBottomLeft:");
+            printTriangleBottomLeft("d", 4);
+            Console.WriteLine();
+
+            //printTriangleTopRight
+            Console.WriteLine("Writing some a:");
+            printTriangleTopLeft("a", 5);
+            Console.WriteLine();
+
+            //printTriangleTopRight
+            Console.WriteLine("von oben nach unten richte seite:");
+            printTriangleTopRight("c", 5);
+            Console.WriteLine();
+
+            //printEmptySquare
+            Console.WriteLine("viereck:");
+            printEmptySquare("ß", 10);
+            Console.WriteLine();
+
+            //printEmptySquare
+            Console.WriteLine("printEmpatySquare");
+            printEmptySquare2("i", 3);
+            Console.WriteLine();
+
+            //printSlash
+            Console.WriteLine("printSlash");
+            printSlash("a", 3);
+            Console.WriteLine();
+
+            //printSlash 2
+            Console.WriteLine("printSlash soll von links weg gehen");
+            printSlash2("d", 9);
+            Console.WriteLine();
+
+            //printTriangleLine
+            Console.WriteLine(" printTriangleLine ");
+            printTriangleLine("d1", 5, 4);
+            Console.WriteLine();
+
+            //printTriangle
+            Console.WriteLine("es soll ein Dreieck raus kommen");
+            printTriangle("v", 3);
+            Console.WriteLine();
+
+            //printTriangle2
+            Console.WriteLine("die gleche würde aber als test durch gefüht");
+            printTriangle2("x", 5);
+            Console.WriteLine();
+
+            //printRhombus
+            Console.WriteLine("Die Größe solldabei die Höhe des Rhombus sein!");
+            printRhombus("i", 7);
+            Console.WriteLine();
+
+        }
+
+
+
+        public static void printChars(string symbol, int count)
+        {
+            for (int x = 0; x < count; x++)
+            {
+                Console.Write(symbol);
+
+
+            }
+
+        }
+
+        public static void printSquare(string symbol, int count)
+        {
+            for (int n = 0; n < count; n++)
+            {
+                printChars(symbol, count);
+                Console.WriteLine();
+
+            }
+        }
+
+        public static void printRect(string symbol, int column, int rows)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                printChars(symbol, column);
+                Console.WriteLine();
+            }
+        }
+
+        public static void printTriangleBottomLeft(string symbol, int rows)
+        {
+            for (int d = 0; d <= rows; d++)
+            {
+                printChars(symbol, d);
+                Console.WriteLine();
+
+            }
+        }
+
+        public static void printTriangleTopLeft(string symbol, int rows)
+        {
+            //for (int a = 0; a > rows; a++)
+            for (int a = rows; a > 0; a--)
+            {
+                //printChars(symbol, rows-a);
+                printChars(symbol, a);
+                Console.WriteLine();
+            }
+        }
+
+        public static void printTriangleTopRight(string symbol, int rows)
+        {
+            for (int c = 0; c < rows; c++)
+            {
+                printChars(symbol, c);
+                Console.WriteLine();
+            }
+        }
+
+        public static void printEmptySquare(string symbol, int rows)
+        {
+
+            printChars(symbol, rows);
+            Console.WriteLine();
+
+
+            for (int e = 0; e < rows - 2; e++)
+            {
+                Console.Write(symbol);
+
+                printChars(" ", rows - 2);
+
+                Console.WriteLine(symbol);
+
+            }
+
+            printChars(symbol, rows);
+        }
+
+
+        public static void printEmptySquare2(string character, int width)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                if (i == 0 || i == width - 1)
+                {
+                    printChars(character, width);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    printChars(character, 1);
+                    printChars(" ", width - 2);
+                    printChars(character, 1);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+
+        public static void printSlash(string symbol, int rows)
+        {
+            for (int a = 0; a < rows; a++)
+            {
+                printChars(" ", a);
+                printChars(symbol, 1);
+
+
+                Console.WriteLine();
+
+
+            }
+        }
+
+        public static void printSlash2(string symbol, int rows)
+        {
+            for (int d = 0; d < rows; d++)
+            {
+                printChars(" ", rows - d);
+                printChars(symbol, -d);
+
+
+                Console.WriteLine(symbol);
+            }
+        }
+
+
+        public static void printTriangleLine(string letter, int lenPrefix, int lenMid)
+        {
+            for (int d1 = 0; d1 <= lenMid; d1++)
+            {
+                printChars(" ", lenMid - d1); // Mit - && + kann ich die richtung ändern...
+                printChars("", lenMid);      // Das macht nicht viel unterschied..
+                Console.WriteLine(letter);
+            }
+        }
+
+
+        /*public static void printTriangle(string letter, int count)
+        {
+            for (int x = 0; x <= count; x++)
+            {
+                printChars(" ", count + x);
+                printChars("", count);
+                Console.WriteLine(letter);
+            }
+        }*/
+
+        public static void printTriangle(string letter, int count)
+        {
+            for (int v = 0; v < count; v++)
+            {
+                for (int j = 0; j < count - v - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+
+                for (int k = 0; k < 2 * v + 1; k++)
+                {
+                    Console.Write(count);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void printTriangle2(string letter, int count)
+        {
+            for (int m = 0; m < count; m++)
+
+            {
+                for (int d = 0; d < count - m - 1; d++)
+                {
+                    printChars(" ", count - d);
+                    Console.Write(" ");
+
+                }
+
+                for (int z = 0; z < 2 * m + 1; z++)
+                {
+                    printChars(" ", count - m);
+                    Console.Write(letter);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void printRhombus(string text, int row)
+        {
+            int spaces = row / 2;
+            int stars = 1;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < stars; k++)
+                {
+                    Console.WriteLine(row);
+                }
+
+                Console.WriteLine();
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Mohammed/Week02/BooleanExamples.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Mohammed.Week02
+{
+    public class BooleanExamples
+    {
+        public static void start()
+        {
+            //SACHEN VON WEEK 1 ZU WIEDER HOLEN
+
+
+            int size = 7;
+
+            for (int i = 0; i < size; i++)
+            {
+
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("x ");
+                    }
+                    else
+                    {
+                        Console.Write(". ");
+                    }
+
+
+
+
+
+                }
+                Console.WriteLine();
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
