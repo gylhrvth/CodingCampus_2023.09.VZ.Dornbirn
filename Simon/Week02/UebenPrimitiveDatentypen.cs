03d51a75d50461465363e4e7006eeba9f0c4de1d
simber01 <simon.bereuter@blum.com>
2023-10-03 09:45:33 +0200

    update

---
+++ Simon/Week02/UebenPrimitiveDatentypen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simon.Week02
+{
+    public class UebenPrimitiveDatentypen
+    {
+        public static void Start()
+        {
+            int size = 7;
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    if(i == j)
+                    {
+                        Console.Write("X ");
+                    }
+                    else
+                    {
+                        Console.Write(". ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
