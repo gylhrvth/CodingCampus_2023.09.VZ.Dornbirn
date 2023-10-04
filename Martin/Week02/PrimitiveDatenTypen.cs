890f941fc23b279fab783cf48016c473435c31ae
Martin <schwendingermartin2@gmail.com>
2023-10-04 15:59:53 +0200

    lol

--- Martin/Week02/PrimitiveDatenTypen.cs
+++ Martin/Week02/PrimitiveDatenTypen.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,69 +12,62 @@ namespace Martin.Week02
     {
         public static void StartDatenTypen()
         {
-            int a = 1;
-            int b = -1;
 
-            Console.WriteLine("ist gleich: " + (a == b));
+            //int Result = Ausrechnen(20);
+            //long Resutl = Faktorial(20);
+            ZeroToHunderd();
 
-            sbyte test = 120;
-            for (int i = 0; i < 20; i++)
+        }
+
+        //Aufgame: Summe
+        public static int Ausrechnen(int number)
+        {
+
+            int result = 0; // ergebnis
+
+            for (int i = number; i <= 50; i++)
             {
-                Console.WriteLine("test2 = " + test);
-                ++test;
-            }
 
-            double d = 5.0;
-            Console.WriteLine("d = " + d);
-            for (int i = 0; i < 20; i++)
-            {
-                d = d + 0.1;
-                Console.WriteLine("d = " + d);
-            }
 
-            int d2 = 500;
-            for (int i = 0; i < 20; i++)
-            {
-                d2 = d2 + 10;
-                Console.WriteLine("d2 = " + (d2 / 100.0));
+                result += i;
+                Console.WriteLine(result);
             }
 
 
+            return result;
 
-            Console.WriteLine("a + 1 = " + (a + 1)); // Addition
-            Console.WriteLine("a - 3 = " + (a - 3)); // Substraktion
-            Console.WriteLine("a * 5 = " + (a * 5)); // Multiplikation
-            Console.WriteLine("a / 5 = " + (a / 5)); // Division (integer)
-            Console.WriteLine("a % 5 = " + (a % 5)); // Modulo (Restwert)
+        }
 
+        //Aufgabe: Faktorial + Erweiterert(Bonus)
 
-            d = 5.25;
-            Console.WriteLine("d + 1 = " + (d + 1)); // Addition
-            Console.WriteLine("d - 3 = " + (d - 3)); // Substraktion
-            Console.WriteLine("d * 5 = " + (d * 5)); // Multiplikation
-            Console.WriteLine("d / 5 = " + (d / 5)); // Division (double)
+        public static long Faktorial(long Number)
+        {
+           long result = 1;
 
-            Console.WriteLine("a = " + a);
-            ++a;
-            Console.WriteLine("a = " + a);
-            a++;
-            Console.WriteLine("a = " + a);
+            for (long i = 1; i <= Number; i++)
+            {
 
+                result = result * i;
 
-            Console.WriteLine("a!!! = " + (a++));
-            Console.WriteLine("a!!! = " + (++a));
+                Console.WriteLine("{0,-2} ! = {1,20} test",i,result);
 
+            }
 
-            a += 5; // a = a + 5
-            Console.WriteLine("a = " + a);
-            a -= 8; // a = a - 8
-            Console.WriteLine("a = " + a);
-            a *= 2; // a = a * 2
-            Console.WriteLine("a = " + a);
-            a /= 5; // a = a / 5
-            Console.WriteLine("a = " + a);
+            return result;
 
+        }
 
+        //Aufgabe: Von 0 bis 100 Zählen in 0.1er Schritten
+
+        public static void ZeroToHunderd()
+        {
+            double number = 0.0;
+
+            for(int i = 0; i < 100; i++)
+            {
+                number += 0.1;
+                Console.WriteLine(number);
+            }
         }
     }
 }
