0ec6ba3f346b344b0a22768661648957f10bbd42
NILA <NILA@ATFKNBNILA>
2023-10-04 15:51:54 +0200

    wesdfgvhnjmk

--- Niklas/Week02/Primitive Datentypen.cs
+++ Niklas/Week02/Primitive Datentypen.cs
@@ -18,6 +18,19 @@ namespace Niklas.Week02
             PrintFaktorial(1);
             Console.WriteLine();
 
+            Console.WriteLine("\nCount: ");
+            Count(0);
+            Console.WriteLine();
+
+            Console.WriteLine("\nCount2: ");
+            Count2(0, 0);
+            Console.WriteLine();
+
+            Console.WriteLine("\nTeilen und Herrschen: ");
+            Teilen(0, 0);
+            Console.WriteLine();
+
+
         }
 
         static void PrintSumme(int times)
@@ -37,37 +50,78 @@ namespace Niklas.Week02
                 rows++;
             }
         }
-        static void PrintFaktorial(int times)
+        static void PrintFaktorial(long times)
         {
-            int rows = 1;
-            int times2 = 2;
+            long rows = 1;
+            long times2 = 1;
             while (rows <= 20)
             {
-                Console.WriteLine($"{times} * {times2} = ");
-                times += times2;
-                times2++;
-                if (times2 % 2 != 0) 
+                if (rows == 2 && times == 1)
                 {
-                    times2++;
-                }
-                if (times % 2 != 1)
-                {
+                    Console.Write("");
                     times++;
                 }
-                rows++;
+                else
+                {
+                    Console.Write($"{rows,2}! = ");
+                    Console.WriteLine($"{times,20}");
+                    times *= times2;
+                    times2++;
+                    if (times % 2 != 0)
+                    {
+                        Console.Write("");
+                    }
+                    else { }
+                    rows++;
+                }
+            }
+
+        }
+        static void Count(double times)
+        {
+            times = 0;
+            Console.WriteLine($"number = {times / 10.0:N1} ");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                times = times + 10.0;
+                Console.WriteLine($"number = {times / 100.0:N1} ");
             }
         }
-    
-    
-    public static int Factor(int times, int times2)
+        static void Count2(double times, double times2)
         {
-            int reulst = times * times2;
+            times = 0;
+            Console.WriteLine($"result 1 digit = {times / 10.0:N1} ");
 
-            return reulst;
+            times = 0;
+            Console.WriteLine($"result 2 digit = {times2 / 100.0,10:N1}0");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                times = times + 10.0;
+                Console.WriteLine($"result 1 digit = {times / 100.0:N1} ");
+
+                times2 = times2 + 100.0;
+                Console.WriteLine($"result 2 digit = {times2 / 1000.00,10:N2} ");
+            }
         }
+
+        static void Teilen(double times, double times2)
+        {
+
+            for (int rows = 0; rows <= 20; rows++)
+            {
+                Console.Write(rows + " / 5 = ");
+                Console.WriteLine();
+                Console.Write(rows + " / 5.0 = ");
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+
+
     }
 }
-
-
-
-
