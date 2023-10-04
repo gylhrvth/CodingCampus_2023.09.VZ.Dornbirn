dd9ce7b85c0ef464609f5d5e680d7f0582cc04c0
Dimitri <github@dmtrh.eu>
2023-10-04 12:29:52 +0200

    add firt solution for Summe

--- Dimitri/Week02/PrimitiveDatentypen.cs
+++ Dimitri/Week02/PrimitiveDatentypen.cs
@@ -10,7 +10,22 @@ namespace Dimitri.Week02
     {
         public static void Main()
         {
+            Summe();
+            
+        }
 
+        public static void Summe()
+        {
+            int sum;
+            int solution = 20;
+            for (int i = 21; i <= 50; i++)
+            {
+                sum = i;
+                solution = sum + solution;
+
+            }
+
+            Console.WriteLine(solution);
         }
     }
 }
