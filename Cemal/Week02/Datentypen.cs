417eb8d4fa740e702bc080cb6319e844512e4871
Cemo38 <cemalkosar38@gmail.com>
2023-10-05 15:50:26 +0200

    TeilenHerrschen

--- Cemal/Week02/Datentypen.cs
+++ Cemal/Week02/Datentypen.cs
@@ -25,7 +25,19 @@ namespace Cemal.Week02
 
 
             Console.WriteLine("Aufgabe Zähler");
-            Zähler1();
+            Zähler();
+            Console.WriteLine();
+
+            Console.WriteLine("Aufgabe Zähler1");
+            double dif2 = Zähler1(100);
+            Console.WriteLine();
+
+            Console.WriteLine("Aufgabe Teilen und Herrschen");
+            TeilenHerrschen();
+            Console.WriteLine();
+
+            Console.WriteLine("Alt version");
+            Alt();
             Console.WriteLine();
         }
 
@@ -44,22 +56,67 @@ namespace Cemal.Week02
             long result = 1;
             for (long i = 1; i <= second; i++)
             {
-                result *= i;                
+                result *= i;
             }
             return result;
         }
 
-        public static void Zähler1()
+        public static void Zähler()
         {
             double first = 0;
-            double second = 100;
+            double second = 10;
             double step = 0.1;
 
             for (double i = first; i <= second; i += step)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(Math.Round(i, 2));
             }
+
             Console.WriteLine();
         }
+
+        public static double Zähler1(double dif)
+        {
+            double first = 0;
+            double second = 10;
+            double step = 0.1;
+
+            for (double i = first; i <= second; i += step)
+            {
+                Console.WriteLine(Math.Round(i, 2));
+            }
+            return dif;
+        }
+        
+        public static void Alt()
+        {
+            double result = 0;
+            string eins = "Result 1 digit = ";
+            string zwei = "Result 2 digit = ";
+
+            for (int i = 0; i <= 100; i++) 
+            {
+                result += 0.1;
+                Console.WriteLine($"{eins}{result:0.0}");
+                Console.WriteLine("{1,0}{0,7:N2}", result, zwei);
+            }
+        }
+
+
+        public static void TeilenHerrschen()
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                string erste = " = ";
+                string zweite = " = ";
+                
+                Console.WriteLine("{0,2} / 5 {1,5}" + i / 5 ,i,erste);
+                Console.WriteLine("{0,2} / 5.0{1,4}" + i / 5.0,i,zweite);
+                Console.WriteLine("---------------");
+
+
+            }
+        }
+
     }
 }
