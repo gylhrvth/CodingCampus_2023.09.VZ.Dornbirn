2229527e27d8503c7179e7483ad671f4bb21091b
davidisele <david.isele88@gmail.com>
2023-10-05 15:55:54 +0200

    New File / Funktions with Returns / Pi

--- David/Week02/PrimitiveDatentypenWeek02Day03.cs
+++ David/Week02/PrimitiveDatentypenWeek02Day03.cs
@@ -8,11 +8,33 @@ namespace David.Week02
             berechnungPi();
 
         }
+        // Funktions with returns / Start Bsp.: von Gyula
+
+        /*public static void Start()
+        {
+            Console.WriteLine(Add(3, 2));
+
+            int c = Add(5, 4);
+            Console.WriteLine(c);
+        }
+
+
+        public static int Add(int a, int b)
+        {
+            int result = a + b;
+
+            return result;
+        }
+       */ 
+
+        //Ende
+        
+
         public static void berechnungPi()
         {
             double pi = 0;
 
-            for (int i = 1; i < 1000000000; i = i + 4)
+            for (int i = 1; i < 10000000; i = i + 4)
             {
                 pi = pi + (4.0 / i);
                 pi = pi - (4.0 / (i + 2));
