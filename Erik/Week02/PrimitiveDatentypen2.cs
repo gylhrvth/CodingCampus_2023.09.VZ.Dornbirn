a4bb24013836509d2d95bcc86e7ec36e8cd788bd
Erik <erikdittrich32@gmail.com>
2023-10-05 15:06:50 +0200

    Calculation of PI (finished)

--- Erik/Week02/PrimitiveDatentypen2.cs
+++ Erik/Week02/PrimitiveDatentypen2.cs
@@ -19,7 +19,9 @@ namespace Erik.Week02
             //CountFromZeroToHundred3();
             //CountFromZeroToHundredExtended();
             //DivideTheNumbers();
-            CalculationOfPi();
+            Console.WriteLine(CalculationOfPi());
+            Console.WriteLine(Math.PI);
+
         }
 
 
@@ -122,47 +124,23 @@ namespace Erik.Week02
             }
         }
 
-        public static void CalculationOfPi()
+        public static double CalculationOfPi()
         {
 
-            double numberPi;
-            double number01;
-            double number02;
-            bool numberChange = true;
-            bool charChange = true;
-
-            for (int i = 1; i < 30000; i += 2)
+            double numberPi = 0;
+            double number01 = 0;
+            double number02 = 0;
+           
+            for (int i = 1; i < 3000000; i += 4)
             {
 
-                if (numberChange)
-                {
-                    number01 = (double)4 / i;
 
-                    numberChange = false;
-                    Console.WriteLine(number01);
-                }
-                else
-                {
-                    number02 = (double)4 / i;
-                    numberChange = true;
-                    Console.WriteLine(number02);
-                }
-
-                if (charChange)
-                {
-
-                    numberPi = number01 - number02;
-                    charChange = false;
-
-                }
-                else
-                {
-                    numberPi = number01 + number02;
-                    numberChange = true;
-                }
-
+                    number01 = (double) 4 / i;                        
+                    number02 = (double) 4 / (i + 2);
+                   
+                    numberPi = numberPi + (number01 - number02);                  
             }
-
+            return numberPi;
 
         }
     }
