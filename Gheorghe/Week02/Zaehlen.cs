29a7d0bb3d0ecd970d8a18c98bf93fa085b08606
Pislea <gheorghepislea26@gmail.com>
2023-10-05 14:54:53 +0200

    Teilen fertig

--- Gheorghe/Week02/Zaehlen.cs
+++ Gheorghe/Week02/Zaehlen.cs
@@ -7,18 +7,22 @@ using System.Threading.Tasks;
 
 namespace Gheorghe.Week02
 {
-    internal class Zählen
+    internal class Zaehlen
     {
         public static void Start()
 
         {
             double d = 0.1;
-            Console.WriteLine("d = " + d);
+           // Console.WriteLine("d = " + d);
+
             for (int i = 0; i <= 100; i++)
 
             {
+
+                Console.WriteLine("Resultdigit 1 = {0,-3:0.0}",d);
+                Console.WriteLine("Resultdigit 2 = {0,7:N2}",d);
                 d = 0.1 + d;
-                Console.WriteLine("d = " + Math.Round(d, 1));
+                // Console.WriteLine("d = " + Math.Round(d, 1));
             }
 
         }
