a5c05b8ea05d9df64b27475b9d3e1a993f2d81a6
Jovo <jovo.drinic@hotmail.com>
2023-10-06 11:33:32 +0200

    Add Pi by Leibnitz

--- Jovo/Week02/Boolean.Examples.cs
+++ Jovo/Week02/Boolean.Examples.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
 namespace Jovo.Week02
 {
     public class Boolean
@@ -9,7 +12,30 @@ namespace Jovo.Week02
             int result = Add();
             Console.WriteLine("ergebniss " + result);
             Console.WriteLine("========================================");
-            Console.WriteLine("aufgabe");
+
+            long result2 = Add2();
+            Console.WriteLine("=========================================");
+
+            int d1 = Zaehlen();
+            Console.WriteLine("ergebniss" + d1);
+            Console.WriteLine("==========================================");
+
+
+
+
+            Console.WriteLine("");
+
+
+
+
+
+
+
+
+
+
+
+
         }
 
 
@@ -18,7 +44,7 @@ namespace Jovo.Week02
         public static void Summe()
         {
             int result = 0;
-            for (int i = 20; i  <= 50; i++) {
+            for (int i = 20; i <= 50; i++) {
                 result += i;
                 Console.WriteLine(result);
             }
@@ -27,54 +53,66 @@ namespace Jovo.Week02
         public static int Add()
         {
             int result = 0;
-            for(int i = 20; i <= 50; i++)
+            for (int i = 20; i <= 50; i++)
             {
                 result += i;
             }
             return result;
         }
 
-        
-        public static void Add2()
+
+        public static long Add2()
         {
-            long result = 0;
-            for(long i = 1; i <= 20; i++)
+
+
+            long result = 1;
+            for (long i = 1; i <= 20; i++)
             {
+
                 result *= i;
-                Console.WriteLine(result);
+                Console.Write($"{i,2}");
+                Console.Write($"{result,20}");
+                Console.WriteLine();
+
+
             }
-            
+            return result;
+
         }
 
+        public static int Zaehlen()
+        {
+
+            int d1 = 100;
+            for (int i = 10; i < 1000; i++)
+            {
+                d1 = d1 + 10;
+                Console.WriteLine(d1 / 100.0);
+            }
+            return d1;
+
+        }
+
+        public static void Zaehlen2()
+        {
+            float d2 = 100;
+            Console.WriteLine(d2);
+            for (int i = 0; i < 100; i++)
+            {
+                d2 = d2 + 10;
+                Console.WriteLine(d2);
+
+            }
 
 
-
-
-
-
-
-
-
-
-
-
-
+        }
+        
 
 
 
 
 
     }
-
-        
-
-
-
-
-
-
-
-
 
     
 
