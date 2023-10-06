5a0c1a5603c4420208a66b9bc3992a47c020774b
Riccardo Ceccon <riccardo.ceccon@blum.com>
2023-10-06 12:35:30 +0200

    .

---
+++ Riccardo/Week02/Arrays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riccardo.Week02
+{
+    internal class Arrays
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
