5c0e25e88ec8cfddbfd07a21443b65fc298f9bc5
Timo Fischer <timo.fischer@blum.com>
2023-10-06 14:24:49 +0200

    End of week 2

--- Timo/Week02/Arrays.cs
+++ Timo/Week02/Arrays.cs
@@ -7,59 +7,90 @@ namespace Timo.Week02
         public static void Start()
         {
             //Aufgabe1.1: Number Array Aufwärts
-            Console.WriteLine("\nNumber1.1 Array\r\n");
-            int[] arrUp = new int[10];
-            for (int i = 0; i < arrUp.Length; i++)
-            {
-                arrUp[i] = i+1;
-            }
+            Console.WriteLine("\nAufgabe1.1: Number Array");
+            int[] arrUp = CreateArrayUp(15);
             for (int i = 0; i < arrUp.Length; ++i)
             {
                 Console.WriteLine("{0}. = {1}", i, arrUp[i]);
             }
 
-            //Aufgabe1.1: Number Array Abwärts
-            Console.WriteLine("\nNumber1.2 Array\r\n");
-            int[] arrDown = new int[10];
-            int a = arrDown.Length;
-            for (int i = 0; i < arrDown.Length; i++)
-            {
-                arrDown[i] = a ;
-                a--;
-            }
+            //Aufgabe1.2: Number Array Abwärts
+            Console.WriteLine("\nAufgabe1.2: Number Array");
+            int[] arrDown = CreateArrayDown(19);
             for (int i = 0; i < arrDown.Length; ++i)
             {
                 Console.WriteLine("{0}. = {1}", i, arrDown[i]);
             }
 
+
             //Aufgabe2
-            Console.WriteLine("\nNumber2 Kopie\r\n");
-            makeACopy(arrUp);
+            //Console.WriteLine("\nAufgabe2: Kopie");
+            //makeACopy(arrUp);
 
 
-            //Aufgabe3
-            Console.WriteLine("\nNumber2 Kopie\r\n");
-            Random random = new Random();
-            
-            int[]arr = new int[10];
-            foreach (int number in arr)
-            {
-                int randomNumber = random.Next(1,100);
-                arr[number] = randomNumber;
-                Console.WriteLine($"{arr[number]} ");
-            }
+            //Aufgabe3.1 Random foreach
+            Console.WriteLine("\nAufgabe3.1: Random Number Array");
+            int[]arr= Random(5, 0, 100);
+
+            //Aufgabe3.1 Random fori
+
         }
 
-        //Aufgabe2: Kopie
-        public static int[] makeACopy(int[] original)
+
+        //Aufgabe1.1: Number Array Aufwärts
+        public static int[] CreateArrayUp(int size)
         {
-            for (int i= 0;i<original.Length; i++) 
-            { 
-                Console.WriteLine(original[i]);
+            int[] arrUp = new int[size];
+            for (int i = 0; i < arrUp.Length; i++)
+            {
+                arrUp[i] = i + 1;
             }
-            return original;
+            return arrUp;
         }
 
-        public static int[] Random(int i, int min, int max);
+
+        //Aufgabe1.2: Number Array Abwärts
+        public static int[] CreateArrayDown(int size)
+        {
+            int[] arrDown = new int[size];
+            int a = arrDown.Length;
+            for (int i = 0; i < arrDown.Length; i++)
+            {
+                arrDown[i] = a;
+                a--;
+            }
+
+            return arrDown;
+        }
+
+    }
+
+
+    /*//Aufgabe2: Kopie
+    public static int[] makeACopy(int[] original)
+    {
+        for (int i = 0; i < original.Length; i++)
+        {
+            Console.WriteLine(original[i]);
+        }
+        return original;
+    }
+    */
+
+
+    //Aufgabe3.1 Random foreach
+    public static int[] Random(int i, int min, int max)
+    {
+        Random random = new Random();
+
+        int[] arr = new int[i];
+        foreach (int number in arr)
+        {
+            int randomNumber = random.Next(min, max);
+            arr[number] = randomNumber;
+            Console.WriteLine($"{arr[number]} ");
+        }
+        return arr;
     }
 }
+
