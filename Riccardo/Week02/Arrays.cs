89ed442ee5c55c17c1165a5c5840efe3ae8a6621
Riccardo Ceccon <riccardo.ceccon@blum.com>
2023-10-06 13:05:01 +0200

    .

--- Riccardo/Week02/Arrays.cs
+++ Riccardo/Week02/Arrays.cs
@@ -10,7 +10,20 @@ namespace Riccardo.Week02
     {
         public static void Start()
         {
+            int[] arr = { 10, 12, 15, 66 };
 
+            Console.WriteLine(arr);
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.WriteLine("{0}. = {1}", i, arr[i]);
+            }
+            Console.WriteLine("===================");
+
+            arr[0] = 100;
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.WriteLine("{0}. = {1}", i, arr[i]);
+            }
         }
     }
 }
