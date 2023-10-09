b1785bfb975b2d69adbaff454fe46313fc70cf20
Selina Burtscher <selina.burtscher@gmx.at>
2023-10-09 17:20:32 +0200

    Array

---
+++ Selina/Week03/Array/Array.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selina.Week03.Array
+{
+    internal class Array
+    {
+        public static void Start()
+        {
+
+        }
+
+    }
+}
