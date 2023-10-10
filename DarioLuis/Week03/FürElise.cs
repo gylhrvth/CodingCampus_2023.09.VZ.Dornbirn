fb31383c8587b02d00d820104d6506fcfcf55a9d
Dario Andrade <dario.andrade566@gmail.com>
2023-10-10 11:31:55 +0200

    Für Elise

---
+++ DarioLuis/Week03/FürElise.cs
@@ -0,0 +1,22 @@
+﻿namespace DarioLuis.Week03
+{
+    class Array
+    {
+        public static void Übungen()
+        {
+            Console.Beep(329, 500);
+            Console.Beep(311, 500);
+            Console.Beep(329, 500);
+            Console.Beep(311, 500);
+            Console.Beep(329, 500);
+            Console.Beep(240, 500);
+            Console.Beep(293, 500);
+            Console.Beep(261, 500);
+            Console.Beep(220, 800);
+            //Console.Beep(261, 500);
+            //Console.Beep(329, 600);
+            //Console.Beep(220, 500);
+
+        }
+    }
+}
