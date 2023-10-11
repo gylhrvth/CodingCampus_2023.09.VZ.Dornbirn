d8882a2fdd9f4a949aaac3a410d7b06391ca360a
Cemo38 <cemalkosar38@gmail.com>
2023-10-11 15:59:00 +0200

    alou

--- Cemal/Week03/Arrays.cs
+++ Cemal/Week03/Arrays.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Cemal.Week03
 {
     internal class Arrays
     {
+       public static Random random = new Random();
         public static void StartArray()
         {
             //Aufgabe 1
@@ -17,15 +19,10 @@ namespace Cemal.Week03
             Console.WriteLine("[{0}]", string.Join(", ", myArr));
             Console.WriteLine();
 
+
             Console.WriteLine("Aufgabe Number Array Rückwärts");
             int[] myArr1 = CreateArray2(6);
             Console.WriteLine("[{0}]", string.Join(", ", myArr1));
-            Console.WriteLine();
-
-
-            //Aufgabe PrintArray
-            Console.WriteLine("Aufgabe PrintArray");
-            PrintArray(myArr);
             Console.WriteLine();
 
 
@@ -33,7 +30,7 @@ namespace Cemal.Week03
             Console.WriteLine();
             Console.WriteLine("Aufgabe Copy");
             int[] copiedArray = MakeaCopy(myArr);
-            foreach (var element in copiedArray)
+            foreach (int element in copiedArray)
             {
                 Console.Write(element + " ");
             }
@@ -42,10 +39,9 @@ namespace Cemal.Week03
 
             myArr[0] = 200;
 
-
             Console.WriteLine();
-            Console.WriteLine("Orginal (verändert)");
-            foreach (var element in myArr)
+            Console.WriteLine("Original (verändert)");
+            foreach (int element in myArr)
             {
                 Console.Write(element + " ");
             }
@@ -61,8 +57,68 @@ namespace Cemal.Week03
             Console.WriteLine();
 
 
-        }
+            //Aufgabe 3 . 1 (mit FOREACH)
 
+            Console.WriteLine();
+            Console.WriteLine("Random Number Array 1.0");
+            int[] myRandom = RandomNumberArray(10);
+            Console.Write("[");
+            foreach (var element in myRandom)
+            {
+                Console.Write(element + ", ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+
+            //Aufgabe 3 . 2 (mit FORi)
+
+            Console.WriteLine();
+            Console.WriteLine("Random Number Array 2.0");
+            for (int i = 0; i < 1; i++)
+            {
+                Console.Write("[{0}]", string.Join(", ", myRandom));
+            }
+            Console.WriteLine();
+
+
+            //Aufgabe 3.3 (Gib den zweiten, fünften und zehnten Wert)
+
+            Console.WriteLine();
+            Console.WriteLine("Random Number Array 3.0");
+            Console.Write(myRandom[1] + ", ");
+            Console.Write(myRandom[4] + ", ");
+            Console.Write(myRandom[9]);
+            Console.WriteLine();
+
+            //Aufgabe 3.4 (Gibt jede zweiten Wert)
+
+            Console.WriteLine();
+            Console.WriteLine("Random Number Array 4.0");
+            for (int i = 0; i < myRandom.Length; i+= 2)
+            {
+                Console.Write(myRandom[i] + ", ");
+            }
+            Console.WriteLine();
+
+
+            //Aufgabe 4
+
+            Console.WriteLine();
+            Console.WriteLine("Random Number Array Crazy Range");
+            foreach (var element in myRandom)
+            {
+
+            }
+
+
+
+
+
+
+
+
+        }
+        ///////////////////////////////////////////////////////////////////////////////////
 
         public static int[] CreateArray(int size)
         {
@@ -74,6 +130,7 @@ namespace Cemal.Week03
             }
             return numbers;
         }
+
 
         public static int[] CreateArray2(int size)
         {
@@ -87,25 +144,35 @@ namespace Cemal.Week03
         }
 
 
-        public static void PrintArray(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i]);
-
-            }
-        }
-
-
         public static int[] MakeaCopy(int[] orginal)
         {
             int[] copied = new int[orginal.Length];
 
-            for (int i = 0;i < copied.Length;i++)
+            for (int i = 0; i < copied.Length; i++)
             {
                 copied[i] = orginal[i];
             }
             return copied;
+        }
+
+        public static int[] RandomNumberArray(int size)
+        {        
+            int[] arr = new int[size];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(101);
+            }
+            return arr;
+        }
+
+        public static int[] RandomNumberArrayCrazy(int size)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-50,50);
+            }
+            return arr;
         }
     }
 }
