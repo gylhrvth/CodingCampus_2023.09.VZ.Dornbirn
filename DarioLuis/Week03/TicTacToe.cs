e9d47faef73bc1dbb0d9d76839ec6f055b55fed0
Dario Andrade <daae@bachmann.at>
2023-10-12 15:46:21 +0200

    TictacToe

--- DarioLuis/Week03/TicTacToe.cs
+++ DarioLuis/Week03/TicTacToe.cs
@@ -1,17 +1,18 @@
-﻿using System;
-
-class TicTacToe
+﻿namespace DarioLuis.Week03
+{
+    class TicTacToe
     {
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static int player = 1; // Spieler 1 startet
         static int choice; // Die Wahl des Spielers
 
         // Überspringt die Anfangsanweisungen
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             bool doExit = false;
             do
             {
+                Solutions();
                 Console.Clear();
                 Console.WriteLine("Spieler 1: X und Spieler 2: O");
                 Console.WriteLine("\n");
@@ -25,6 +26,7 @@ class TicTacToe
                 }
                 Console.WriteLine("\n");
                 Board();
+                Solutions();
 
                 // Überprüfung, welcher Spieler gewonnen hat
                 if (player % 2 == 0)
@@ -54,7 +56,9 @@ class TicTacToe
                         Console.WriteLine("Leider ist die Zeile {0} bereits mit einem {1} belegt.", choice, arr[choice]);
                         Console.WriteLine("\n");
                     }
+
                 }
+                Solutions();
             }
             while (!doExit);
 
@@ -74,6 +78,25 @@ class TicTacToe
             Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
             Console.WriteLine("     |     |      ");
         }
+        public static void Solutions()
+        {
+            if (((arr[1] == arr[5]) && (arr[5] == arr[9]))    //Diagonale 1 bis 9
+            || ((arr[3] == arr[5]) && (arr[5] == arr[7]))     //Diagonale 3 bis 7
+            || ((arr[1] == arr[4]) && (arr[4] == arr[7]))     //Abwärts 1 bis 7
+            || ((arr[2] == arr[5]) && (arr[5] == arr[8]))     //Abwärts 2 bis 8
+            || ((arr[3] == arr[6]) && (arr[6] == arr[9]))     //Abwärts 3 bis 9
+            || ((arr[1] == arr[2]) && (arr[2] == arr[3]))     //Horizontal von 1 bis 3
+            || ((arr[4] == arr[5]) && (arr[5] == arr[6]))     //Horizontal von 4 bis 6
+            || ((arr[7] == arr[8]) && (arr[8] == arr[9])))   //Horizontal von 7 bis 9
+            {
+                Console.WriteLine("Jemand hat gewonnen");
+            }
+            else
+            {
+                Console.WriteLine("Es hat noch keiner gewonnen!!");
+            }
+        }
     }
 }
+
 
