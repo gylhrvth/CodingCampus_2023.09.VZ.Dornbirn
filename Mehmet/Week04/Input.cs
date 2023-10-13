05d871261df705ccd6afb6cdae257a1541604176
Mehmet <kalay.mehmet@outlook.com>
2023-10-13 12:07:07 +0200

    OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

---
+++ Mehmet/Week04/Input.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mehmet.Week04
+{
+    internal class Input
+    {
+        public static void Start()
+        {
+
+            //String input = Console.ReadLine();
+            //Console.WriteLine("your input is: " + input);
+
+
+            //int input = Console.Read();
+            //Console.WriteLine("your input is: " + input);
+
+
+            //int readNumber = ReadNumbner("Enter a Numero:", " EROR 404 : Try again : ");
+
+            int readRandomNumber = ReadRandomNumber("Las uns ein Spiel spielen ö_ö , tippe einen Zahl ein :_[", " Versuchs doch mit einem Zahl: ", "höher: ", "runta: ", "Richtig du Pic!!!!");
+
+
+        }
+
+        public static Random rand = new Random();
+        static int ReadNumbner(String yes, String no)
+        {
+            Console.WriteLine(yes);
+            while (true)
+            {
+                try
+                {
+                    String input = Console.ReadLine();
+                    int myNumber = Convert.ToInt32(input);
+                    return myNumber;
+                }
+                catch
+                {
+                    Console.Error.WriteLine(no);
+                }
+            }
+        }
+
+
+        static int ReadRandomNumber(String Stage1,String error ,String higher, String lower, String correct)
+        {
+            Console.WriteLine(Stage1);
+            int random = rand.Next(101);
+            int answer = random;
+            while (true)
+            {
+                try
+                {
+                    String input = Console.ReadLine();
+                    int myNumber = Convert.ToInt32(input);
+
+                    if (answer < myNumber)
+                    {
+                        Console.WriteLine(lower);
+                    }
+                    else if (answer > myNumber)
+                    {
+                        Console.WriteLine(higher);
+                    }
+                    else if (answer == myNumber)
+                    {
+                        Console.WriteLine(answer + " " + correct);
+                        return answer;
+                    }
+                            
+                }
+                catch
+                {
+                    Console.Error.WriteLine(error);
+                }
+            }
+
+
+        }
+
+
+
+
+    }
+}
