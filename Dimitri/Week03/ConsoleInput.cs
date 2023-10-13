aa69ba68e5ff5000bb61c814193f664f20d515ab
Dimitri <github@dmtrh.eu>
2023-10-13 10:10:04 +0200

    Solution to first console input example

---
+++ Dimitri/Week03/ConsoleInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimitri.Week03
+{
+    internal class ConsoleInput
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Write your input here:");
+            StringOutput(StringRead());
+        }
+
+        public static string StringRead()
+        {
+            string input = Console.ReadLine();
+            return input;
+
+        }
+
+        public static void StringOutput(string input)
+        {
+            Console.WriteLine("This is your input:");
+            Console.WriteLine(input);
+
+        }
+    }
+}
