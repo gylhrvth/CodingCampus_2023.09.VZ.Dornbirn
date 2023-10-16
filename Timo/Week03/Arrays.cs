b3ba7369969a93b7a06dd3ef42ddb52a76b1c448
Timo Fischer <timo.fischer@blum.com>
2023-10-16 10:36:50 +0200

    .

--- Timo/Week03/Arrays.cs
+++ Timo/Week03/Arrays.cs
@@ -304,7 +304,6 @@ namespace Timo.Week03
         //------------------------------------------------------------------------------------------------------------------------------
 
         //Aufgabe7.1 Min.
-        //ToDo 7.1 leeres Array!
         public static int Minimum(int[] arr)
         {
             int min = Int32.MaxValue;
@@ -330,9 +329,7 @@ namespace Timo.Week03
             }
         }
 
-        //Aufgabe7.2 Max.,
-        //ToDo 7.2 leeres Array!
-
+        //Aufgabe7.2 Max.
         public static int Maximum(int[] arr)
         {
             int max = Int32.MinValue;
@@ -353,8 +350,6 @@ namespace Timo.Week03
         }
 
         //Aufgabe7.3 Avg
-        //ToDo 7.3 leeres Array!
-
         public static double Average(int[] arr)
         {
             double avg = (ArraySum(arr) * 1.00) / arr.Length;
@@ -368,8 +363,6 @@ namespace Timo.Week03
         }
 
         //Aufgabe7.4 IndexMin
-        //ToDo 7.4 leeres Array!
-
         public static int IndexMinimum(int[] arr)
         {
             int indexMin = 0;
@@ -384,8 +377,6 @@ namespace Timo.Week03
         }
 
         //Aufgabe7.5 IndexMax
-        //ToDo 7.5cd leeres Array!
-
         public static int IndexMaximum(int[] arr)
         {
             int indexMax = 0;
--- Timo/Week03/ConsoleInput.cs
+++ Timo/Week03/ConsoleInput.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using Timo.Week01;
 
 namespace Timo.Week03
 {
@@ -24,13 +26,18 @@ namespace Timo.Week03
             Console.WriteLine("Read number is: " + readNumber);
 
             Console.WriteLine("=======================================");
+            
+
+            //Aufgabe3+4 Ratespiel
+            Console.WriteLine("Aufgabe3+4: Ratespiel");
+            Guessinggame(0,100, "Ich will nicht mehr");
+
+            Console.WriteLine("=======================================");
             */
 
-            //Aufgabe3 Ratespiel
-            Console.WriteLine("Aufgabe3: Ratespiel");
-            Guessinggame(ReadNumber("Please enter your number:", "Thats not a number. Please try again:"));
-
-
+            //Aufgabe5 Menü
+            Console.WriteLine("Aufgabe5: Menü");
+            Menue("exit");
         }
 
         //Aufgabe1
@@ -40,14 +47,34 @@ namespace Timo.Week03
             return myInput;
         }
 
-
         //------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+        //Ergänzung ReadChar
+        public static char ReadChar(String prompt, String onError)
+        {
+            Console.WriteLine(prompt);
+            while (true)
+            {
+                try
+                {
+                    String input = Console.ReadLine();
+                    char myChar = Convert.ToChar(input);
+                    return myChar;
+                }
+                catch (FormatException exc)
+                {
+                    Console.Error.WriteLine(onError);
+                }
+            }
+        }
+        //------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+
 
         //Aufgabe2
         public static int ReadNumber(String prompt, String onError)
         {
             Console.WriteLine(prompt);
-
             while (true)
             {
                 try
@@ -68,28 +95,117 @@ namespace Timo.Week03
 
         //------------------------------------------------------------------------------------------------------------------------------
 
-        //Aufgabe3
-        public static void Guessinggame(int input)
+        //Aufgabe3+4 Ratespiel
+        public static void Guessinggame(int startNumber, int endNumber, String exitCode)
         {
+            Console.WriteLine("Welcome to the game! Try to guess the correct number: enter a number between {0} and {1}. \nWrite '{2}' to leave the game", startNumber, endNumber, exitCode);
+            Random random = new Random();
             int count = 1;
-            int i = new Random().Next(1, 101);
-
-            while (input != i)
+            int i = random.Next(startNumber, endNumber + 1);
+            while (true)
             {
-                int zahl = input;
-                if (i < zahl)
+                try
                 {
-                    Console.WriteLine("Your number is too big, try again");
+                    String input = Console.ReadLine();
+                    string exit = exitCode;
+                    if (input == exit)
+                    {
+                        Console.WriteLine("Sad to see you going, hope to see you soon :)");
+                        return;
+                    }
+                    int output = Convert.ToInt32(input);
+                    if (output == i)
+                    {
+                        Console.WriteLine("You have won! Number of guesses: " + count);
+                        return;
+                    }
+                    else if (output < i)
+                    {
+                        Console.WriteLine("Your number is too small, try again");
+                    }
+                    else if (output > i)
+                    {
+                        Console.WriteLine("Your number is too big, try again");
+                    }
                 }
-                else if (i > zahl)
+                catch
                 {
-                    Console.WriteLine("Your number is too small, try again");
-                }
-                else
-                {
-                    Console.WriteLine("You have won! Number of guesses: " + count);
+                    Console.WriteLine("Please choose an integer ");
                 }
                 count++;
+            }
+        }
+
+
+        //------------------------------------------------------------------------------------------------------------------------------
+
+        //Aufgabe5 Menü
+        public static void Menue(string exitCode)
+        {
+            Console.WriteLine("Welcome!");
+            while (true)
+            {
+                Console.WriteLine("What do you want to draw ? \n1) Square \n2) Pyramid \n3) Christmastree \nWhat do you want to choose? ");
+                string k = "Please choose an valid integer";
+                string o = "What character should be used?";
+                string l = "Please choose an valid character";
+                try
+                {
+                    String input = Console.ReadLine();
+                    string exit = exitCode;
+                    if (input == exit)
+                    {
+                        Console.WriteLine("Sad to see you going, hope to see you soon :)");
+                        return;
+                    }
+                    int output = Convert.ToInt32(input);
+                    if (output == 1)
+                    {
+                        MethodenUndSchleifen.PrintSquare(ReadChar(o, l), ReadNumber("What should be the size of the square?", k));
+                        Console.WriteLine("new?");
+                        int x = 1; 
+                        while (x==1)
+                        {
+                            try
+                            {
+                                String answerIn = Console.ReadLine();
+                                char answer = Convert.ToChar(answerIn);
+
+                                if (answer=='j')
+                                {
+                                    Console.WriteLine("haha");
+                                    x = 0;
+                                }
+                                else if (answer == 'n')
+                                {
+                                    return;
+                                }
+                                else 
+                                {
+                                    Console.WriteLine("Please enter 'j' or 'n'");
+                                }
+                            }
+                            catch { Console.WriteLine("Please enter 'j' or 'n'"); }
+
+                        }
+                    }
+                    else if (output == 2)
+                    {
+                        MethodenUndSchleifen.PrintPyramid(ReadChar(o, l), ReadNumber("What should be the size of the pyramid?", k));
+                    }
+                    else if (output == 3)
+                    {
+                        MethodenUndSchleifen.PrintChristmasTree(ReadChar(o, l), ReadNumber("What should be the size of the christmastree?", k));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please choose an valid integer");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please choose an valid integer");
+                }
             }
         }
     }
