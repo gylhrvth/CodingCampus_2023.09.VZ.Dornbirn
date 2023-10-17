2112976289b562464de7a050b1ba681eb565c878
Cemo38 <cemalkosar38@gmail.com>
2023-10-17 15:59:57 +0200

    asdasd

--- Cemal/Program.cs
+++ Cemal/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Cemal.Week02.BooleanExamples.StartBooleanExamples();
 //Cemal.Week01.Methoden_Schleifen.Start();
 //Cemal.Week02.Datentypen.StartDatentypen();
-Cemal.Week03.Arrays.StartArray();
+//Cemal.Week03.Arrays.StartArray();
+Cemal.Week04.ConsoleInput.StartInput();
