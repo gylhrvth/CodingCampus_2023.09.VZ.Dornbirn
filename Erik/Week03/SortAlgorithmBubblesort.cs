75ab0c8402c6796d84a881897f7c67bb2453a3a5
Erik <erikdittrich32@gmail.com>
2023-10-17 11:42:09 +0200

    Bubblesort Increasing and Decreasing

--- Erik/Week03/SortAlgorithmBubblesort.cs
+++ Erik/Week03/SortAlgorithmBubblesort.cs
@@ -7,13 +7,32 @@ namespace Erik.Week03
     {
         public static void StartSortAlgorithm()
         {
-            int[] arr = NumbersOutput(-150, 100, 20);
+            int[] arr = NumbersOutput(0, 100, 20);
             Console.WriteLine("Unsorted numbers:");
             PrintArrayWithIndex(arr);
-            Console.WriteLine("Sorted numbers");
-            SortAlgoBubblesort(arr);
-            PrintArrayWithIndex(arr);
+            Console.WriteLine();
+            Console.WriteLine("< " + "for increasing sort /" + " >" + " for decreasing sort");
+            Console.Write("Your Input: ");
+            String userDecision = Console.ReadLine();
 
+
+
+            if (userDecision == "<")
+            {
+                Console.WriteLine("Sorted numbers increasing:");
+                SortAlgoBubblesortIncrease(arr);
+                PrintArrayWithIndex(arr);
+            }
+            else if (userDecision == ">")
+            {
+                Console.WriteLine("Sorted numbers decreasing:");
+                SortAlgoBubblesortDecrease(arr);
+                PrintArrayWithIndex(arr);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
         }
 
         public static int[] NumbersOutput(int randMin, int randMax, int randSize)
@@ -27,7 +46,8 @@ namespace Erik.Week03
             return arr1;
         }
 
-        public static void SortAlgoBubblesort(int[] arr)
+
+        public static void SortAlgoBubblesortIncrease(int[] arr)
         {
             int n = arr.Length;
 
@@ -36,6 +56,24 @@ namespace Erik.Week03
                 for (int j = 0; j < n - 1 - i; j++)
                 {
                     if (arr[j] > arr[j + 1])
+                    {
+                        int tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                    }
+                }
+            }
+        }
+
+        public static void SortAlgoBubblesortDecrease(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (arr[j] < arr[j + 1])
                     {
                         int tmp = arr[j];
                         arr[j] = arr[j + 1];
