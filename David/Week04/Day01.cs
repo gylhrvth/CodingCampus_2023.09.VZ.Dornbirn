423801b6fd39591b2e51ab5d6dcf44c210b6028e
davidisele <david.isele88@gmail.com>
2023-10-17 11:57:25 +0200

    bubblesort1AufgabeFin

--- David/Week04/Day01.cs
+++ David/Week04/Day01.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using static David.Week03.Week03Day03RandomArrays;
+
 namespace David.Week04
 {
-	public class Day01
-	{
-		public static void StartDay01()
-		{
+    public class Day01
+    {
+        public static void StartDay01()
+        {
+            int[] arr = RandomA(20);
+            
 
-		}
-	}
+            BubbleSort(arr);
+            Console.WriteLine("Sorted:");
+            for (int i = 0; i < 1; i++)
+            {
+                Console.Write("[{0}]", string.Join(", ", arr));
+            }
+            Console.WriteLine();
+
+
+        }
+
+        public static int[] BubbleSort(int[] array)
+        {
+            int i = 0;
+            int j = 0;
+
+            for (j = 0; j < array.Length; j++)
+            {
+                for (i = 0; i < array.Length - j - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+    
 }
-
