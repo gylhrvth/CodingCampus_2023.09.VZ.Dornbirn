d3cbff9727c27d8c3a3b271fd83c6f3a736b9666
Sven Juen <juensv@gmail.com>
2023-10-18 12:14:30 +0200

    ok

--- Sven/Week04/RekursiveUebungen.cs
+++ Sven/Week04/RekursiveUebungen.cs
@@ -11,21 +11,54 @@ namespace Sven.Week04
             RekursiveSumme(4);
             Console.WriteLine();
 
+            Console.WriteLine("Rekursive Faktorial:");
+            RekursiveFaktorial(10);
+            Console.WriteLine();
+
+            Console.WriteLine("Rekursive Fibonacci:");
+            RekursiveFibonacci(15);
+            Console.WriteLine();
+
         }
 
-        public static int RekursiveSumme(int number)
+        static int RekursiveSumme(int number)
         {
             if (number == 0)
             {
-                return number;
+                return 0;
             }
 
-            else
-            {
-                int sum = number + RekursiveSumme(number - 1);
-            }
-            return number;
+            int sum = number + RekursiveSumme(number - 1);
+            Console.WriteLine(sum);
+            return sum;
+
         }
+
+        static int RekursiveFaktorial(int Fakt)
+        {
+            if (Fakt == 1)
+            {
+                return 1;
+            }
+
+            int sum = Fakt * RekursiveFaktorial(Fakt - 1);
+            Console.WriteLine(sum);
+            return sum;
+
+        }
+        static int RekursiveFibonacci(int fib)
+        {
+            if (fib == 1 || fib == 2)
+            {
+                return 1;
+            }
+
+            return fib = RekursiveFibonacci(fib - 1) + RekursiveFibonacci (fib - 2);
+            //Console.WriteLine(sum);
+            //return sum;
+
+        }
+
 
     }
 }
