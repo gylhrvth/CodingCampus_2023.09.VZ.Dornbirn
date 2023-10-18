75ea5ed5cae294fd3258ac470ac054abf37c7247
Erik <erikdittrich32@gmail.com>
2023-10-18 10:19:59 +0200

    FaktorialCalculation

---
+++ Erik/Week04/FaktorialCalculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erik.Week04
+{
+    public class FaktorialCalculation
+    {
+
+        public static void Start()
+        {
+
+
+        }
+
+        public static int SumOfNumbers(int n)
+        {
+            if (n == 0) return 0;
+
+            return n + SumOfNumbers(n - 1);
+        }
+
+        public static void SumOfNumbersFaktorial(int context)
+        {
+
+
+
+        }
+    }
+}
