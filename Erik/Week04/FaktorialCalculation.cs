93baee9924ddea9311c27c7356d09448a36024ae
Erik <erikdittrich32@gmail.com>
2023-10-18 12:27:10 +0200

    GuessingGame

--- Erik/Week04/FaktorialCalculation.cs
+++ Erik/Week04/FaktorialCalculation.cs
@@ -11,22 +11,41 @@ namespace Erik.Week04
 
         public static void Start()
         {
+            Console.WriteLine(SumOfNumbers);
+            Console.WriteLine(FaktorialSum);
+            Console.WriteLine(Fibonacci);
 
 
         }
 
         public static int SumOfNumbers(int n)
         {
-            if (n == 0) return 0;
-
-            return n + SumOfNumbers(n - 1);
+            int num = 0;
+            int sum = num + (num + 1);
+            return sum;
         }
 
-        public static void SumOfNumbersFaktorial(int context)
+
+        public static int recursionSum(int context)
         {
+            {
+                if (context == 0) return 0;
+                return context + SumOfNumbers(context - 1);
+            }  
+        }
 
+        public static int FaktorialSum(int context)
+        {
+            {
+                if (context == 1) return 1;
+                return context * SumOfNumbers(context - 1);
+            }
+        }
 
-
+        public static int Fibonacci(int x)
+        {
+            if (x == 1 || x == 2) return 1;
+            x = Fibonacci(x - 1) + Fibonacci(x - 2); return x;
         }
     }
 }
