9d4b454392f64851758ef3ea66c9117eef8d0808
Sven Juen <juensv@gmail.com>
2023-10-19 10:23:26 +0200

    New

--- Sven/Week04/UserInputUebungen.cs
+++ Sven/Week04/UserInputUebungen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
@@ -12,8 +13,12 @@ namespace Sven.Week04
             StringRead();
             Console.WriteLine();
 
-            Console.WriteLine("String Read:");
+            Console.WriteLine("String Read2:");
             StringRead2();
+            Console.WriteLine();
+
+            Console.WriteLine("Guess Game:");
+            GuessGame();
             Console.WriteLine();
 
         }
@@ -50,6 +55,67 @@ namespace Sven.Week04
                 catch
                 {
                     Console.WriteLine("Bitte nur Zahlen eingeben!");
+                }
+            }
+
+        }
+
+        public static void GuessGame()
+        {
+
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            string response;
+
+            while (playAgain)
+            {
+                guess = 0;
+
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    try
+                    {
+                        Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Guess: " + guess);
+
+                        if (guess > number)
+                        {
+                            Console.WriteLine(guess + " is to high!");
+                        }
+                        else if (guess < number)
+                        {
+                            Console.WriteLine(guess + " is to low!");
+                        }
+                    }
+                    catch
+                    {
+                        
+                    }
+                   
+
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("YOU WIN!");
+
+                Console.WriteLine("Would you like to play again (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
                 }
             }
 
