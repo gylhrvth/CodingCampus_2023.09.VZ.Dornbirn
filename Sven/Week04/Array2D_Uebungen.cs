dbdcf708944e64b021207f939a88570db85dda74
Sven Juen <juensv@gmail.com>
2023-10-20 11:37:25 +0200

    2d Array

--- Sven/Week04/Array2D_Uebungen.cs
+++ Sven/Week04/Array2D_Uebungen.cs
@@ -14,11 +14,18 @@ namespace Sven.Week04
         {
             Console.WriteLine("Create 2DArray:");
             int[][] arr2d = Create2DArray(3,5);
+            Print2DArray(arr2d);
             Console.WriteLine();
 
-            //Console.WriteLine("Create 2DArray:");
-            //Print2DArray(arr2d);
-            //Console.WriteLine();
+            Console.WriteLine("Sum Row 2D Array:");
+            int[] sumRow = SumByRow(arr2d);
+            Console.WriteLine(string.Join(" ", sumRow));
+            Console.WriteLine();
+
+            Console.WriteLine("Sum Col 2D Array:");
+            int[] sumCol = SumByCol(arr2d);
+            Console.WriteLine(string.Join(" ", sumCol));
+            Console.WriteLine();
 
 
 
@@ -42,15 +49,6 @@ namespace Sven.Week04
                     arr2d[row][col] = random.Next(10, 100);
                 }
             }
-
-            for (int row = 0; row < arr2d.Length; row++)
-            {
-                for (int col = 0; col < arr2d[row].Length; col++)
-                {
-                    Console.Write(arr2d[row][col] + " ");
-                }
-                Console.WriteLine();
-            }
             return arr2d;
         }
         public static void Print2DArray(int[][] arr2d)
@@ -66,6 +64,38 @@ namespace Sven.Week04
                 Console.WriteLine();
 
             }
+        }
+
+        public static int[] SumByRow(int[][] arr)
+        {
+            int[] result = new int[arr.Length];
+
+            for ( int i = 0; i < arr.Length; i++)
+            {
+                for ( int j = 0; j < arr[i].Length; j++)
+                {
+                    result[i] += arr[i][j];
+                }
+            }
+            return result;
+        }
+
+        public static int[] SumByCol(int[][] arr)
+        {
+            if (arr.Length == 0)
+            {
+                return new int[] { };
+            }
+
+            int[] result = new int[arr[0].Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    result[j] += arr[i][j];
+                }
+            }
+            return result;
         }
 
     }
