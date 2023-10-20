239ed5e7d7612531d5b114703bea8ac3330f09c1
Dimitri <github@dmtrh.eu>
2023-10-20 12:39:43 +0200

    add 2d array and pascaltraingle

--- Dimitri/Week04/TwoDArray.cs
+++ Dimitri/Week04/TwoDArray.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Dimitri.Week03.Array;
+using static Dimitri.Week03.ConsoleInput;
+using static Dimitri.Week04.ConsoleInputPart2;
 
 namespace Dimitri.Week04
 {
@@ -11,24 +14,47 @@ namespace Dimitri.Week04
     {
         public static void Main()
         {
-            PrintArray2d(Array2dRandom(2));
+            //PrintArray2d(Array2dRandom(2));
+            //PrintArray2d(Array2dRandom(5, 5));
+            //int[][] a = Array2dRandom(5, 5);
+            //PrintArray2d(a);
+            //Console.WriteLine();
+            //SumArray2D(a); //, true);
+            //Console.WriteLine();
+            //int[] sumRow = SumRow(a);
+            //Console.WriteLine(string.Join(" ", sumRow));
+            //Console.WriteLine();
+            //int[] sumCol = SumCol(a);
+            //Console.WriteLine(string.Join(" ", sumCol));
+            int[][] arr = PascalTriangle(10);
+            PrintPascalTriangle(arr);
+
         }
 
-        public static int[][] Array2dRandom(int rows)
+
+        public static int[][] Array2dRandom(int numRow, int numCol)
         {
-            int[][] array = new int[rows][];
+            int[][] array = new int[numRow][];
 
             Random random = new Random();
 
-            for (int row = 0; row < rows; row++)
+            for (int i = 0; i < numRow; i++)
             {
-                for (int col = 0; col < array[row].Length; col++)
+                array[i] = new int[numCol];
+            }
+
+            for (int j = 0; j < numRow; j++)
+            {
+
+
+                for (int k = 0; k < numCol; k++)
                 {
-                    array[row][col] = random.Next(10, 100);
+                    array[j][k] = random.Next(0, 101);
                 }
             }
 
             return array;
+
         }
 
         public static void PrintArray2d(int[][] array)
@@ -38,11 +64,138 @@ namespace Dimitri.Week04
             {
                 for (int col = 0; col < array[row].Length; col++)
                 {
-                    Console.Write(array[row][col] + " ");
+                    Console.Write("{0} ", array[row][col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void SumArray2D(int[][] array)
+        {
+
+            Console.WriteLine("You wanna have the sum of rows or columns? (r/c)");
+            string input = AskYN(StringRead(), "r", "c");
+
+            bool sumRow = false;
+            if (input.CompareTo("r") == 0)
+            {
+                sumRow = true;
+            }
+
+            Console.WriteLine();
+
+            if (sumRow)
+            {
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int sum = 0;
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        sum += array[i][j];
+
+                    }
+                    Console.WriteLine("Sum of {0}. row is {1}.", i + 1, sum);
+                }
+            }
+            else if (!sumRow)
+            {
+                int index = 0;
+
+                while (index < array[0].Length)
+                {
+                    int sum = 0;
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        sum += array[i][index];
+                    }
+
+                    Console.WriteLine("Sum of {0}. column is {1}.", index + 1, sum);
+                    index++;
+
+                }
+            }
+
+        }
+
+        public static int[] SumRow(int[][] array)
+        {
+            int[] sum = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    sum[i] += array[i][j];
+
+                }
+                ;
+            }
+
+            return sum;
+        }
+
+        public static int[] SumCol(int[][] array)
+
+        {
+            if (array.Length == 0)
+            {
+                return new int[] { };
+            }
+
+            int[] sum = new int[array[0].Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    sum[j] += array[i][j];
+                }
+            }
+            return sum;
+        }
+
+        public static int[][] PascalTriangle(int input)
+        {
+            int[][] array = new int[input][];
+
+            for (int i = 0; i < input; i++)
+            {
+                array[i] = new int[input];
+            }
+
+            // fill first 3 values
+
+            for (int j = 0; j < input; j++)
+            {
+                for (int k = 0; k < input; k++)
+                {
+                    array[j][k] = 1;
+                }
+            }
+
+            for (int j = 1; j < input; j++)
+            {
+
+                for (int k = 1; k < input; k++)
+                {
+                    array[j][k] = array[j - 1][k] + array[j][k - 1];
+                }
+            }
+
+            return array;
+
+        }
+
+        public static void PrintPascalTriangle(int[][] array)
+        {
+            for (int row = 0; row < array.Length; row++)
+            {
+                for (int col = 0; col < array[row].Length; col++)
+                {
+                    Console.Write("{0,5} ", array[row][col]);
                 }
                 Console.WriteLine();
             }
         }
     }
-
 }
