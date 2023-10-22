818cda8ac7e1bee0b5b46f588da5d33d51035011
Fabii137 <krsticfabian@gmail.com>
2023-10-22 23:59:08 +0200

    smooth Game of life

--- Fabian/Week04/GameOfLifeClass.cs
+++ Fabian/Week04/GameOfLifeClass.cs
@@ -1,44 +1,21 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Text;
+﻿using System.Text;
 
 namespace Fabian.Week04
 {
     public class GameOfLifeClass
     {
-
-        private static readonly Random random = new Random();
+        public static readonly Random random = new();
 
         public static void Start()
         {
-            Thread gameThread = new(GameOfLife);
-            gameThread.Start();
+            GameOfLife();
         }
 
         public static void GameOfLife()
         {
             int[,] matrix = new int[100, 100];
 
-            //fill array with 0's
-            /*for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    matrix[i, j] = 0;
-                }
-            }
-            
-            //glider       
-            matrix[0, 1] = 1;
-            matrix[1, 2] = 1;
-            matrix[2, 0] = 1;
-            matrix[2, 1] = 1;
-            matrix[2, 2] = 1;
-            */
-            
-            //fill matrix with random nums between 0 - 1
+            // fill array with random nums between 0 - 1
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -47,21 +24,21 @@ namespace Fabian.Week04
                 }
             }
 
-            PrintMatrix(matrix);
             while (true)
             {
                 matrix = ChangeMatrix(matrix);
 
+                PrintMatrix(matrix);
                 try
                 {
-                    Thread.Sleep(100);
+                    Thread.Sleep(10);
                 }
                 catch (ThreadInterruptedException) { }
 
-                Console.Clear();
-                PrintMatrix(matrix);
+
             }
         }
+
 
         public static int[,] ChangeMatrix(int[,] matrix)
         {
@@ -71,7 +48,7 @@ namespace Fabian.Week04
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    int alive = CountAliveNeighbors(matrix, i, j);               
+                    int alive = CheckAliveNeighbors(matrix, i, j);
 
                     if (matrix[i, j] == 1)
                     {
@@ -87,30 +64,32 @@ namespace Fabian.Week04
                     }
                     else
                     {
-                        // rule 1
+                        //rule 1
                         if (alive == 3)
                         {
                             changedMatrix[i, j] = 1;
                         }
-                    }                
+                    }
                 }
             }
+
+
             return changedMatrix;
         }
 
-        public static int CountAliveNeighbors(int[,] matrix, int x, int y)
+        public static int CheckAliveNeighbors(int[,] matrix, int x, int y)
         {
             int alive = 0;
-            int iStart = Int32.Max(0, x - 1);
-            int iEnd = Int32.Min(x + 2, matrix.GetLength(0));
 
-            int jStart = Int32.Max(0, y - 1);
-            int jEnd = Int32.Min(y + 2, matrix.GetLength(1));
+            int iStart = Math.Max(0, x - 1);
+            int iEnd = Math.Min(x + 2, matrix.GetLength(0));
+            int jStart = Math.Max(0, y - 1);
+            int jEnd = Math.Min(y + 2, matrix.GetLength(1));
 
             for (int i = iStart; i < iEnd; i++)
             {
                 for (int j = jStart; j < jEnd; j++)
-                {                  
+                {
                     if (i != x || j != y)
                     {
                         alive += matrix[i, j];
@@ -120,9 +99,10 @@ namespace Fabian.Week04
             return alive;
         }
 
-        public static int[,] PrintMatrix(int[,] matrix)
+        public static void PrintMatrix(int[,] matrix)
         {
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new();
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -135,7 +115,7 @@ namespace Fabian.Week04
                     {
                         //Console.ForegroundColor = ConsoleColor.Green;
 
-                        ConsoleColor randomColor = (ConsoleColor)random.Next(1, 16); //random colors / avoid black(0)
+                        ConsoleColor randomColor = (ConsoleColor)random.Next(1, 15);
                         Console.ForegroundColor = randomColor;
 
                         sb.Append((char)9632 + " ");
@@ -143,9 +123,9 @@ namespace Fabian.Week04
                 }
                 sb.AppendLine();
             }
-            Console.WriteLine(sb.ToString());
 
-            return matrix;
+            Console.SetCursorPosition(0, 0);
+            Console.Write(sb.ToString());
         }
     }
 }
