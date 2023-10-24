de03fe70fb834316f30418c227bd6463be4f58da
Gyula Horvath <gylhrvth@gmail.com>
2023-10-24 08:19:18 +0200

    Add String Ressources

--- Gyula/Week05/StringExample.cs
+++ Gyula/Week05/StringExample.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using Ressources;
+
 namespace Gyula.Week05
 {
 	public class StringExample
 	{
 		public static void Start()
 		{
-			
+			Console.WriteLine(StringRessources.getText());
         }
     }
 }
