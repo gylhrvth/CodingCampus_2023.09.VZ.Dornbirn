dd2292abcecd2f827cd4dc907fbf5b91c7c80459
Sven Juen <juensv@gmail.com>
2023-10-24 13:54:58 +0200

    Strings

--- Sven/Week05/StringUebungen.cs
+++ Sven/Week05/StringUebungen.cs
@@ -9,7 +9,91 @@ namespace Sven.Week05
     internal class StringUebungen
     {
 
+        public static void Start()
+        {
+
+            String[] names = new String[] { "Jovo", "Mehmet", "Sven", "Martin", "Selina",
+                "Niklas", "Ali", "Fabienne", "Lukas", "Sandro", "Hassan", "Berna", "Gyula",
+                "Dimitri", "Patrick", "Kerem", "Timo", "Gheorghe", "Mohammed", "Cemal",
+                "Simon", "Fabian", "Dario", "Michael", "Erik", "David", "Riccardo", "Eren" };
+
+            Console.WriteLine("Bubblesort String Ascending:");
+            Console.WriteLine("{0}", string.Join(", ", AscendNames(names)));
+            Console.WriteLine();
+
+            Console.WriteLine("Bubblesort String Descending:");
+            Console.WriteLine("{0}", string.Join(", ", DescendNames(names)));
+            Console.WriteLine();
+
+            Console.WriteLine("Bubblesort String Lexicographic Ascending:");
+            Console.WriteLine("{0}", string.Join(", ", LexicoAscend(names)));
+            Console.WriteLine();
+
+            
+
+        }
+        public static string[] AscendNames(string[] arr)
+        {
+            string temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j].Length > arr[j + 1].Length)
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        public static string[] DescendNames(string[] arr)
+        {
+            string temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j].Length < arr[j + 1].Length)
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+        public static string[] LexicoAscend(string[] arr)
+        { 
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (string.Compare(arr[j], arr[j + 1]) > 0)
+                    {
+                        string temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+      
+                
+            }
+            return arr;
+        }
 
 
     }
 }
+
+
+
+
+
+
+
