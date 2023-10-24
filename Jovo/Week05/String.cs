2b0ead1fb2f7de39851b8ecc86af6154f325791c
Jovo <jovo.drinic@hotmail.com>
2023-10-24 14:50:47 +0200

    new

--- Jovo/Week05/String.cs
+++ Jovo/Week05/String.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using static Jovo.Week04.ConsoleInput;
 
 namespace Jovo.Week05
 {
@@ -43,8 +45,22 @@ namespace Jovo.Week05
             Console.WriteLine("==============================");
             PrintArray(names);
 
+            Console.WriteLine("==============================");
 
+            int mynumber = ReadNumber("Wähle 1 für BubbleSortAscending  oder 2 für BubbleSortDescending");
 
+            BubbleSortStringExtended(names , (mynumber == 1));
+            Console.WriteLine("BubbleSortStringExtended");
+            Console.WriteLine("==============================");
+            BubbleSortStringExtended(names, true); 
+
+            CharIndex(names , 2);
+            Console.WriteLine("CharIndex");
+            Console.WriteLine("==============================");
+            PrintArray(names);
+
+            Console.WriteLine("==================================");
+            Console.WriteLine(names[0][0]);
         }
 
 
@@ -129,6 +145,40 @@ namespace Jovo.Week05
             }
 
         }
+
+
+        public static void BubbleSortStringExtended(string[] arr, bool ascending)
+        {
+           if (ascending)
+           {
+               BubbleSortAscending(arr);
+           }
+           else
+           {
+                BubbleSortDescending(arr);
+           }
+        }
+
+
+        public static void CharIndex(string[] arr, int Index)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j][Index] > arr[j + 1][Index])
+                    {
+                        string temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+
+        }
+
 
 
 
