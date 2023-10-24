35008f7aaf2444ebf7da073b38ef82e487e0bb0f
davidisele <david.isele88@gmail.com>
2023-10-24 16:00:29 +0200

    end of day

--- David/Program.cs
+++ David/Program.cs
@@ -11,5 +11,5 @@
 //David.Week04.Array2D2.Start();
 //David.Week04.arrayWE.Start();
 //David.Week04.Day02CocktailShakerSort.Start();
-David.Week05.Strings.StartDay01();
-
+//David.Week05.Strings.StartDay01();
+David.Week05.ReverseProgramm.StartReverseProgramm();
