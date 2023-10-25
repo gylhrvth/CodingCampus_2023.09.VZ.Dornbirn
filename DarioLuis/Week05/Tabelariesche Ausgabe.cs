de8f99426823dd0215d46b72c8903f51ce0857cd
Dario Andrade <daae@bachmann.at>
2023-10-25 15:51:14 +0200

    Week Nachmittag-Do

---
+++ DarioLuis/Week05/Tabelariesche Ausgabe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarioLuis.Week05
+{
+    internal class Tabelariesche_Ausgabe
+    {
+    }
+}
