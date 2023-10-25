8240f081e09d7f080a5604ae809136457c9eb807
ZaynHassan7 <zaynhassan9@gmail.com>
2023-10-25 11:48:53 +0200

    Compare

--- Hassan/Week05/TabellarischeAusgabe.cs
+++ Hassan/Week05/TabellarischeAusgabe.cs
@@ -21,16 +21,67 @@ namespace Hassan.Week05
         }
         static void PrintTable(string[] firstNames, string[] lastNames, int[] ages, string[] places, float[] distances)
         {
-            Console.WriteLine("----------------------------------------------------------------------------------------------");
-            Console.WriteLine("|{0,-20}|{1,-20}|{2,-5}|{3,-20}|{4,-20}|", "Vorname", "Nachname", "Alter", "Ort", "Entfernung von Wien (km)");
-            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            // if the length of the highest value is bigger than the length of the name of the column = take the length of the highest value
+            // else = take the length of the name of the column
+            int firstNameLength = Math.Max(9,GetMaxLengthS(firstNames));
+            int lastNameLength = Math.Max(8,GetMaxLengthS(lastNames));
+            int ageMaxLength = Math.Max(3,GetMaxLengthI(ages));
+            int placeMacLenght = Math.Max(5,GetMaxLengthS(places));
+            int distanceMaxLenghth = Math.Max(21,GetMaxLengthF(distances));
 
+            int totalLength = firstNameLength + lastNameLength + ageMaxLength + placeMacLenght + distanceMaxLenghth + 12; //12 for the seperators
+            string line = new string('-', totalLength);
+
+            //printtabel
+            Console.WriteLine(line);
+            Console.WriteLine($"Firstname  {new string(' ', firstNameLength - 9)} | Lastname{new string(' ', lastNameLength - 8)} | Age{new string(' ', ageMaxLength - 3)} | Place{new string(' ', placeMacLenght - 5)} | Distance from Capital");
+            Console.WriteLine(line);
             for (int i = 0; i < firstNames.Length; i++)
             {
-                Console.WriteLine("|{0,-20}|{1,-20}|{2,-5}|{3,-20}|{4,-20}|", firstNames[i], lastNames[i], ages[i], places[i], distances[i]);
+                Console.WriteLine($"{firstNames[i]}{new string(' ', firstNameLength - firstNames[i].Length)} | {lastNames[i]}{new string(' ', lastNameLength - lastNames[i].Length)} | {ages[i]}{new string(' ', ageMaxLength - ages[i].ToString().Length)} | {places[i]}{new string(' ', placeMacLenght - places[i].Length)} | {distances[i]}");
+
             }
 
-            Console.WriteLine("----------------------------------------------------------------------------------------------");
+        }
+
+        public static int GetMaxLengthS(String[] arr)
+        {
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length > max)
+                {
+                    max = arr[i].Length;
+                }
+            }
+            return max;
+        }
+
+
+        public static int GetMaxLengthI(int[] arr)
+        {
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].ToString().Length > max)
+                {
+                    max = arr[i].ToString().Length;
+                }
+            }
+            return max;
+        }
+
+        public static int GetMaxLengthF(float[] arr)
+        {
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].ToString().Length > max)
+                {
+                    max = arr[i].ToString().Length;
+                }
+            }
+            return max;
         }
 
     }
