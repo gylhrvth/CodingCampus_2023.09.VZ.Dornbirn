da384caf22cb1e642d1bf083c7332dcca5860c70
Erik <erikdittrich32@gmail.com>
2023-10-25 14:33:12 +0200

    point out a word WIP

--- Erik/Program.cs
+++ Erik/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Erik.Week05;
 
 
-CheckEmailRegex.Start();
+PointOutAWord.Start();
--- Erik/Week05/SearchingForWord.cs
+++ Erik/Week05/SearchingForWord.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 using System.Linq.Expressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Erik.Week05
 {
@@ -7,18 +9,26 @@ namespace Erik.Week05
     {
         public static void Start()
         {
-            
+            SearchForWord("Hesse");
         }
 
         public static void SearchForWord(String wordToSearch)
         {
             String randomText = Ressources.StringRessources.getHesse();
+            int wordCounter = 0;
+            int index = randomText.IndexOf(wordToSearch);
 
 
-
-
-            
-
+            while (index != -1)
+            {
+                wordCounter++;
+                Console.Write("Word counter: " + wordCounter + "\n>>>at the position: " + index + "\n" + "");
+                index = randomText.IndexOf(wordToSearch, index + 1);
+            }
+            if (wordCounter == 0)
+            {
+                Console.WriteLine("No word found!");
+            }
         }
     }
 }
