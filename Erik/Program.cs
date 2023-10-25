a6dfa6a9079d1535ab9581b6deccebffa5d59fcd
Erik <erikdittrich32@gmail.com>
2023-10-25 12:06:57 +0200

    searching for word WIP

--- Erik/Program.cs
+++ Erik/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Erik.Week05;
 
 
-Emailregex.Start();
+CheckEmailRegex.Start();
---
+++ Erik/Week05/SearchingForWord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Erik.Week05
+{
+    public class SearchingForWord
+    {
+        public static void Start()
+        {
+            
+        }
+
+        public static void SearchForWord(String wordToSearch)
+        {
+            String randomText = Ressources.StringRessources.getHesse();
+
+
+
+
+            
+
+        }
+    }
+}
