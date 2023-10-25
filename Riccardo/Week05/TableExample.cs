a10d77bcbf2be053c98715dda31b2cefc04c7ba0
Riccardo Ceccon <riccardo.ceccon@blum.com>
2023-10-25 15:56:23 +0200

    TableExample

---
+++ Riccardo/Week05/TableExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riccardo.Week05
+{
+    internal class TableExample
+    {
+        public static void Start()
+        {
+
+        }
+
+    }
+}
