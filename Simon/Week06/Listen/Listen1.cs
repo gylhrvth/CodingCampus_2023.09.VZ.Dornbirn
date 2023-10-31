6b1e37378cb16a0fe1b3615c3fc072813010832e
simber01 <simon.bereuter@blum.com>
2023-10-31 15:02:23 +0100

    Personen

---
+++ Simon/Week06/Listen/Listen1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simon.Week06.Listen
+{
+    public class Listen1
+    {
+        private int[] _Arr;
+        public int[] Arr { get => _Arr; }
+
+        public Listen1(int[] Arr)
+        {
+            _Arr = Arr;
+        }
+
+
+
+
+
+    }
+}
---
+++ Simon/Week06/PersonExample/PersonData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simon.Week06.PersonExample
+{
+    public class PersonData
+    {
+        public static void Start()
+        {
+            Person p1 = new Person("Max", 11);
+            Person p2 = new Person("Till", 34);
+
+            Console.WriteLine(p1.Height);
+            p1.Height = 1.84;
+            p1.Weight = 84;
+            p2.Height = 1.02;
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            Kurse k1 = new Kurse("Code FAST LEARN FAST", DateTime.Now, 500, 9700, p2);
+            Console.WriteLine(k1);
+        }
+    }
+}
