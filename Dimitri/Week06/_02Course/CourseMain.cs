e4e69f30055156db04a2f56b0a393030e4563a9e
Dimitri <github@dmtrh.eu>
2023-10-31 13:21:19 +0100

    rename folders and classes so classes have a different name than the namespace for simpler usage

--- Dimitri/Week06/_02Course/CourseMain.cs
+++ Dimitri/Week06/_02Course/CourseMain.cs
@@ -10,19 +10,21 @@ namespace Dimitri.Week06.Course
     {
         public static void Main()
         {
-            Person.Person p = new Person.Person(174, 31, "Dimitri", 64);
-            //Course c = new Course("Softwareentwickler", "26.09.2023", 500, 7840.99, p);
-            Course c = new Course("Softwareentwickler", "26.09.2023", 500, p);
+            Person.Person p = new(174, 31, "Dimitri", 64);
+            Course c = new("Softwareentwickler", "26.09.2023", 500, 7840.99, p);
+            Course c1 = new Course("Softwareentwickler", "26.09.2023", 500, p);
 
             Console.WriteLine(c);
+            Console.WriteLine();
+            Console.WriteLine(c1);
             Console.WriteLine();
             Console.WriteLine(c.Title);
             c.Title = "Angehender Softwareentwickler";
             Console.WriteLine(c.Title);
             Console.WriteLine();
-            Console.WriteLine(c.Price);
-            c.Price = 9999;
-            Console.WriteLine(c.Price);
+            //Console.WriteLine("Preis alt: " + c1._Price);
+            //c1._Price = 9999;
+            //Console.WriteLine("Preis neu: " + c1._Price);
 
         }
     }
