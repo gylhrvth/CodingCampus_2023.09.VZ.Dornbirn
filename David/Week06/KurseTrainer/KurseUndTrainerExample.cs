b33730414690a160afd25a3bfdf6ef9f4133798e
davidisele <david.isele88@gmail.com>
2023-11-01 16:22:33 +0100

    Sort examples: List auswerten

---
+++ David/Week06/KurseTrainer/KurseUndTrainerExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using David.Week06.PersonExample;
+namespace David.Week06.KurseTrainer
+{
+	public class KurseUndTrainerExample
+	{
+		public static void Start()
+		{
+            KurseUndTrainer K1 = new KurseUndTrainer("Software Developer", "26. 09. 2023", "€ 7.850,-", 500);
+
+            Console.WriteLine(K1);
+
+          
+        }
+	}
+}
+
