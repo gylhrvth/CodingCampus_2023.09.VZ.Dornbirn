c9349dbcc0597afa4c62bea248d52fc4d017bc46
Cemo38 <cemalkosar38@gmail.com>
2023-11-03 09:28:18 +0100

    NumbersList

--- Cemal/Program.cs
+++ Cemal/Program.cs
@@ -5,7 +5,14 @@
 //Cemal.Week04.ConsoleInput.StartInput();
 //Cemal.Week04.RecursiveExamples.Start();
 //Cemal.Week05.String_Aufgaben.Start();
+//Cemal.Week06.Person.PersonMain.Start();
+//Cemal.Week06.Kurse_und_Trainer.KursMain.Start();
+Cemal.Week06.NmsList.Numbers.Start();
 
-using Cemal.Week06.Person;
-    
-PersonMain.Start();
+//using Cemal.Week06.NmsList;
+
+//List<int> values = Numbers.RandomNumbers(10);
+//Numbers.PrintList(values);
+
+//values.Remove(2);
+//Numbers.PrintList(values);
