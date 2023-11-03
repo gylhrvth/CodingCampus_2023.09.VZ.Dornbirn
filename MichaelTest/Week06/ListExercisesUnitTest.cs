a80256ad6296fd7b3f5bf9dce1c03937184466f0
Gyula Horvath <gylhrvth@gmail.com>
2023-11-03 13:55:22 +0100

    Add MichaelTest

---
+++ MichaelTest/Week06/ListExercisesUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Michael.Week06;
+
+namespace MichaelTest.Week06
+{
+	[TestClass]
+	public class ListExercisesUnitTest
+	{
+		[TestMethod]
+		public void CountEvenEmptyList()
+		{
+            // arrange
+            List<int> emptyList = new List<int>();
+
+            // act
+            
+
+            // assert
+        }
+    }
+}
+
