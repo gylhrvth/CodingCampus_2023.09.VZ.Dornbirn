75d0d05100fa4d664d1e5001aedd8aabe4436230
Dario Andrade <daae@bachmann.at>
2023-11-06 08:33:46 +0100

    Halloooo

---
+++ DarioLuis/Week06/EnumExamples.cs
@@ -0,0 +1,32 @@
+﻿namespace DarioLuis.Week06
+{
+    public class EnumExamples
+    {
+        public enum WeekDays
+        {
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY,
+        }
+        public static void Start()
+        {
+            WeekDays w = WeekDays.MONDAY;
+            for (int i = 0; i < 7; i++)
+            {
+                Printname();
+                ++w;    
+            }
+        }
+        public static void Printname() 
+        {
+            WeekDays w = WeekDays.MONDAY;
+            Console.WriteLine(w++);
+            ++w;
+            
+        }
+    }
+}
