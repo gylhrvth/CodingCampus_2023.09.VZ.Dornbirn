e9dbc2b6d82de0cbc0567725cf76787741a6d7b2
Fabii137 <krsticfabian@gmail.com>
2023-11-09 08:53:56 +0100

    hospital

---
+++ Fabian/Week07/05_Hospital/Hospital.cs
@@ -0,0 +1,39 @@
+﻿namespace Fabian.Week07._05_Hospital
+{
+    public class Hospital
+    {
+        private string _Name;
+        private List<Department> _Departments;
+
+        public List<Department> Departments
+        {
+            get => _Departments;
+            set => _Departments = value;
+        }
+        public string Name
+        {
+            get => _Name; 
+        }
+        public Hospital(string Name)
+        {
+            _Name = Name;
+            _Departments = new List<Department>();
+        }
+
+        public void PrintStructure()
+        {
+            Console.WriteLine($"├── Hospital: {_Name}");
+            foreach (var department in _Departments)
+            {
+                foreach (var station in department.Stations)
+                {
+                    station.PrintStations();
+                }
+                foreach (var ambulance in department.Ambulances)
+                {
+                    ambulance.PrintAmbulances();
+                }
+            }
+        }
+    }
+}
---
+++ Fabian/Week07/05_Hospital/Station.cs
@@ -0,0 +1,49 @@
+﻿namespace Fabian.Week07._05_Hospital
+{
+    public class Station : Department
+    {
+        private List<Patient> _Patients;
+        private List<Room> _Rooms;
+
+        public Station(string name, List<Room> rooms)
+        {
+            _Name = name;
+            _Rooms = rooms;
+            _Patients = new List<Patient>();
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            bool patientAdded = false;
+            foreach (var room in _Rooms)
+            {
+                if (room.Beds > room.BedsTaken)
+                {
+                    _Patients.Add(patient);
+                    patient.Room = room;
+                    room.BedsTaken++;
+                    Console.WriteLine($"{patient.Name} got added to room {room.RoomNumber}");
+                    patientAdded = true;
+                    break;
+                }               
+            }
+            if (!patientAdded) Console.WriteLine($"No available space in any room for {patient.Name}");
+        }
+
+        public void PrintStations()
+        {
+            Console.WriteLine($"│\t├── Station: {Name}");
+            foreach (var room in _Rooms)
+            {
+                Console.WriteLine($"│\t\t├── Room {room.RoomNumber}:");
+                foreach (var patient in _Patients)
+                {
+                    if (patient.Room == room)
+                    {
+                        patient.PrintPatients();
+                    }
+                }
+            }
+        }
+    }
+}
