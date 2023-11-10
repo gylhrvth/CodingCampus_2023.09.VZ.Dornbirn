0ccc03ac4a013aaa0b8bc772978e27eaa3fb4886
Dimitri <github@dmtrh.eu>
2023-11-10 10:13:22 +0100

    add changes

--- Dimitri/Week06/_05Zoo/ZooMain.cs
+++ Dimitri/Week06/_05Zoo/ZooMain.cs
@@ -8,9 +8,10 @@ using System.Threading.Tasks;
 namespace Dimitri.Week06._05Zoo
 {
     internal class ZooMain
-    {
+    { 
         public static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8;
             List<Gehege> gehegeList = new();
             Zoo myZoo = new("Zoo Goefis", 1991, gehegeList);
 
@@ -27,16 +28,20 @@ namespace Dimitri.Week06._05Zoo
 
             Futter Hühnchen = new("Hühnchen", "kg", 5.99);
 
+            Futter Korn = new("Sonnenblumenkerne", "kg", 1.95);
+
+            Futter Zuckerwasser = new("Fruchtsaft", "l", 1.29);
+
             Tier emil = new("Emil", "Mitteleuropäische Kurzhaar", Hühnchen, 1);
             Gehege.AddTier(katzenGehege, emil);
             Tier sandy = new("Sandy", "Mitteleuropäische Kurzhaar", Hühnchen, 0.75);
             Gehege.AddTier(katzenGehege, sandy);
 
-            Tier sauerFlatter = new("Sauerflatter", "Zitronenfalter");
+            Tier sauerFlatter = new("Sauerflatter", "Zitronenfalter", Zuckerwasser, 0.001);
             Gehege.AddTier(schmetterlingsGehege, sauerFlatter);
 
 
-            Tier rotKehle = new("Mitrotemhals", "Rotkehlchen");
+            Tier rotKehle = new("Mitrotemhals", "Rotkehlchen", Korn, 0.01);
             Gehege.AddTier(vogelGehege, rotKehle);
 
             Zoo.AddGehege(myZoo, new Gehege("Terrarium", new List<Tier>()));
