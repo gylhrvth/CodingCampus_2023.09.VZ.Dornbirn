128c3802c57bcda649914702daa3cdf11333252b
davidisele <david.isele88@gmail.com>
2023-11-11 17:05:42 +0100

    -zoo

--- David/Week06/ZOO/Animals.cs
+++ David/Week06/ZOO/Animals.cs
@@ -5,13 +5,11 @@ namespace David.Week06.ZOO
 {
     public class Animals
     {
-
-
         private string _Name;
         private int _Age;
         private string _Genus;
         private string _Gender;
-
+        private List<Food> _Food;
 
         public Animals(string name, int age, string genus, string gender)
         {
@@ -19,11 +17,29 @@ namespace David.Week06.ZOO
             _Age = age;
             _Genus = genus;
             _Gender = gender;
+            _Food = new List<Food>();
         }
 
-        public void PrintZoo()
+        public void AddFood(Food food)
         {
-            Console.WriteLine($" === === {_Name}, {_Age}, {_Genus}, {_Gender}");
+            if (!_Food.Contains(food))
+            {
+                _Food.Add(food);
+            }
+        }
+
+        public void PrintAnimals()
+        {
+            if (_Food != null)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine($"      ||--> Name: {_Name}, Age: {_Age}, Type: {_Genus}, Sex: {_Gender}");
+
+                foreach (Food foo in _Food)
+                {
+                    foo.PrintFood();
+                }
+            }
         }
 
     }
