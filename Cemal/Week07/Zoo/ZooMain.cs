6319231cb860a9fee6f5d81ee2c2e4e9303a4237
Cemo38 <cemalkosar38@gmail.com>
2023-11-14 13:46:53 +0100

    Add Zoo v0.1

---
+++ Cemal/Week07/Zoo/ZooMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cemal.Week07.Zoo
+{
+    public class ZooMain
+    {
+        public static void Start()
+        {
+            Zoo zoo = new("Tiergarten Dornbirn", 2022);
+            zoo.PrintStructure();
+        }
+
+    }
+}
