e2d3456c75fedbf63a79956dc2f5f7960fd88f11
Sven Juen <juensv@gmail.com>
2023-11-14 14:15:10 +0100

    alrighty

---
+++ Sven/Week07/Gehege.cs
@@ -0,0 +1,23 @@
+﻿namespace Sven.Week07
+{
+    internal class Enclosure
+    {
+
+        private string _Name;
+        private string _Description;
+        private string _Climate;
+
+        public Enclosure(string description, string climate, string name)
+        {
+            _Description = description;
+            _Climate = climate;
+            _Name = name;
+
+        }
+        public void PrintZoo()
+        {
+            Console.WriteLine($" --- Enclosure: {_Description}, {_Climate}, {_Name}");
+
+        }
+    }
+}
