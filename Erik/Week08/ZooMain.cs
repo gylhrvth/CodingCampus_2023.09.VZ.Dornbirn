8bc52be47055ca8a520bc02bd0da0a89ebaac237
Erik <erikdittrich32@gmail.com>
2023-11-15 12:27:18 +0100

    Zoo update

--- Erik/Week08/ZooMain.cs
+++ Erik/Week08/ZooMain.cs
@@ -8,11 +8,15 @@ namespace Erik.Week08
         {
             Zoo DoppelmayrZoo = new Zoo("Doppelmayr Zoo Dornbirn", 2003);
 
+            //Food                   Name /  Unit / Price
             Food Banana = new Food("Banana", 700, 100);
             Food Meat = new Food("Meat", 1500, 350);
-            Food Fruits = new Food("Fruits", 500, 50);;
+            Food Fruits = new Food("Fruits", 500, 50);
             Food Grass = new Food("Gras", 20000, 500);
+            Banana.FoodList(Banana);
 
+
+            
             Keeper Keeper1 = new Keeper("Johannes");
             Keeper Keeper2 = new Keeper("Peter");
             Keeper Keeper3 = new Keeper("Jasmin");
@@ -27,32 +31,32 @@ namespace Erik.Week08
             Animal Steffen = new Animal("Steffen", "Mountain Zebra", "Male", Fruits);
             Animal Sara = new Animal("Sara", "Mountain Zebra", "Female",Fruits);
 
-            Enclosure MonkeyEnclosure = (new Enclosure("Monkey", 500));
-            Enclosure LionEnclosure = (new Enclosure("Lion", 780));
-            Enclosure ElephantEnclosure = (new Enclosure("Elephant", 530));
-            Enclosure TigerEnclosure = (new Enclosure("Tiger", 150));
-            Enclosure SpiderEnclosure = (new Enclosure("Spider", 5));
-            Enclosure ZebraEnclosure = (new Enclosure("Zebra", 450));
+            Enclosure MonkeyEnclosure = (new Enclosure("Monkey", 500, Keeper1));
+            Enclosure LionEnclosure = (new Enclosure("Lion", 780, null));
+            Enclosure ElephantEnclosure = (new Enclosure("Elephant", 530, Keeper2));
+            Enclosure TigerEnclosure = (new Enclosure("Tiger", 150, Keeper3));
+            Enclosure SpiderEnclosure = (new Enclosure("Spider", 5, null));
+            Enclosure ZebraEnclosure = (new Enclosure("Zebra", 450, Keeper3));
 
             //Monkey Enclosure
             MonkeyEnclosure.AddAnimals(Martin);
             MonkeyEnclosure.AddAnimals(Michael);
-            ZebraEnclosure.AddKeeperToEnclosure(Keeper3);
+
 
             //Elephant Enclosure
             ElephantEnclosure.AddAnimals(Thomas);
-            ZebraEnclosure.AddKeeperToEnclosure(Keeper2);
+
 
             //Tiger Enclosure
             TigerEnclosure.AddAnimals(Susanne);
             TigerEnclosure.AddAnimals(Luis);
-            ZebraEnclosure.AddKeeperToEnclosure(Keeper1);
+
             //Zebra Enclosure
             ZebraEnclosure.AddAnimals(Peter);
             ZebraEnclosure.AddAnimals(Sabine);
             ZebraEnclosure.AddAnimals(Steffen);
             ZebraEnclosure.AddAnimals(Sara);
-            ZebraEnclosure.AddKeeperToEnclosure(Keeper2);
+
 
             //Lion Enclosure
             //Spider Enclosure
@@ -69,6 +73,7 @@ namespace Erik.Week08
             DoppelmayrZoo.PrintZoo("├──");
 
             PressToContinue.Start("Press Enter to feed the Animals!");
+            
 
 
 
