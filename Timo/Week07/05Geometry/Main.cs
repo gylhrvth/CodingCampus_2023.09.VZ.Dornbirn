7ebc90157e0b6d00de4896f9888e85d8ec914c20
Timo Fischer <timo.fischer@blum.com>
2023-11-15 15:52:43 +0100

    end of day

---
+++ Timo/Week07/05Geometry/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timo.Week07._05Geometry
+{
+    internal class Main
+    {
+        public static void Start() 
+        {
+            Square s1 = new(10.5);
+
+        }
+    }
+}
