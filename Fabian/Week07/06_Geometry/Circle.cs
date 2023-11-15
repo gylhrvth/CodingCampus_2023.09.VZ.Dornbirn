860d9521f6ac0c7eaa0060a211b20f26e29391c9
Fabii137 <krsticfabian@gmail.com>
2023-11-15 15:53:53 +0100

    autoooooooooooformeeeen

---
+++ Fabian/Week07/06_Geometry/Circle.cs
@@ -0,0 +1,22 @@
+﻿namespace Fabian.Week07._06_Geometry
+{
+    public class Circle : Geometry
+    {
+        private double _Radius;
+
+        public Circle(double length)
+        {
+            _Radius = length;
+        }
+
+        public override double GetCircumference()
+        {
+            return 2 * Math.PI * _Radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.Pow(_Radius, 2) * Math.PI;
+        }
+    }
+}
