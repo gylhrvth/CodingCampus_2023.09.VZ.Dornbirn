69d07f225614591407678ec3b96c1661b5b73043
ZaynHassan7 <zaynhassan9@gmail.com>
2023-11-15 11:31:27 +0100

    OOP

--- Hassan/Week06/Person.cs
+++ Hassan/Week06/Person.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,22 +9,36 @@ namespace Hassan.Week06
 {
     public class Person
     {
-        private String name;
+        private string _Name;
+        public String Name {
+            get => _Name;
+            set
+            {
+                if (value != "")
+                {
+                    _Name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Name must be non-empty.");
+                }
+            }
+        }
         private int age;
         private int height;
         private double weight;
 
         public Person(String name, int age, int height, double weight)
         {
-            this.name = name;
+            this._Name = name;
             this.age = age;
             this.height = height;
             this.weight = weight;
         }
 
-        public String toString()
+        public override String ToString()
         {
-            return "Name    : " + name + "\n" +
+            return "Name    : " + _Name + "\n" +
                     "Age     : " + age + " years old" + "\n" +
                     "Height  : " + height + " cm" + "\n" +
                     "Weight  : " + weight + "kg" + "\n";
@@ -32,13 +47,10 @@ namespace Hassan.Week06
 
 
 
-
-
-
         public static void Start()
         {
             Person person = new Person("Hassan", 21, 188, 80);
-            Console.WriteLine(person.toString());
+            Console.WriteLine(person);
         }
 
     }
