44f424f29c95e4a277a99839b5bc0b5d9d70ff10
AliAsuk <asuk_ali@icloud.com>
2023-11-15 15:55:33 +0100

    zoo

---
+++ Ali/Week06/Kurs/KursExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ali.Week06.Kurs
+{
+    public class KursExample
+    {
+        public static void Start()
+        {
+            Kurs1 k1 = new Kurs1("Software Dev", "26.September.2023", 459, 7890, "Gyula");
+
+            Console.WriteLine(k1.ToString());
+        }
+    }
+
+}
---
+++ Ali/Week06/Kurs/KursPerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ali.Week06.Kurs
+{
+    public class KursPerson
+    {
+        private string _Name;
+        private int _Age;
+        private int _Height;
+        private int _Weight;
+
+        public string Name
+        {
+            get => "\"" + _Name + "\"";
+        }
+        public KursPerson(string name, int age, int height, int weigth)
+        {
+            _Name = name;
+            _Age = age;
+            _Height = height;
+            _Weight = weigth;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Person: {0}, age: {1}", _Name, _Age);
+        }
+    }
+}
