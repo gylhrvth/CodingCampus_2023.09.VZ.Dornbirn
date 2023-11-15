322566c4613a09e1dc9d2fc5998b22a0fc3dee62
Riccardo Ceccon <riccardo.ceccon@blum.com>
2023-11-15 15:15:07 +0100

    ZooDoppelmayer

---
+++ Riccardo/Week07/ZooDoppelmayer/Zoo.cs
@@ -0,0 +1,48 @@
+﻿using Riccardo.Week07.ZooDoppelmayer;
+using System;
+
+namespace Riccardo.Week07.ZooDoopelmayer
+{
+    public class Zoo
+    {
+        private string _Name;
+        private int _Established;
+        private List<Enclosure> _EnclosureList;
+
+        public Zoo(string name, int established)
+        {
+            _Name = name;
+            _Established = established;
+            _EnclosureList = new List<Enclosure>();
+        }
+
+        public void PrintStructure()
+        {
+            Console.WriteLine("├── Zoo: {0}, gegründet {1}",
+                _Name,
+                _Established);
+            foreach (Enclosure enc in _EnclosureList)
+            {
+                enc.PrintStructure();
+            }
+        }
+        
+        public Enclosure SearchAndCreate(string name)
+        {
+            foreach (Enclosure enc in _EnclosureList)
+            {
+                if (enc.Name == name)
+                {
+                    return enc;
+                }
+            }
+            Enclosure newenc = new Enclosure(name);
+            _EnclosureList.Add(newenc);
+            return newenc;
+        }
+        
+
+    }
+
+}
+
