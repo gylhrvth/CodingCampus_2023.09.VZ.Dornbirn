09372e704ce9501c5961f2e57420ec3764a71f67
Paepm <mair.patrick@gmx.at>
2023-11-16 16:01:34 +0100

    zoo

---
+++ Patrick/Week06/Zoo/VeterinarySurgeon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrick.Week06.Zoo
+{
+    internal class VeterinarySurgeon
+    {
+        private string _Name;
+        
+
+        public VeterinarySurgeon(string Name)
+        {
+            _Name = Name;
+        }
+
+
+
+
+
+    }
+}
