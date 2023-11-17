47e3e6191f111bc96aec31dd191ee029cf4fd105
Sven Juen <juensv@gmail.com>
2023-11-17 15:06:13 +0100

    ok

---
+++ Sven/Week07/Vererbung/MittelKlasseAutos.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Sven.Week07.Modelierung
+{
+    public class MittelKlasseAutos : Auto
+    {
+
+        private int _Price;
+
+        public MittelKlasseAutos( string name , string color, int hp, int price) : base (name, color, hp)
+        {
+            _Price = price;
+        }
+
+
+
+    }
+}
