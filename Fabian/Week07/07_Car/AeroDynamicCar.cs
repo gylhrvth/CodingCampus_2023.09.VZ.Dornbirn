f6edf96cb6afc43439b38dc08a095e14293b3396
Fabii137 <krsticfabian@gmail.com>
2023-11-20 07:53:04 +0100

    ya

--- Fabian/Week07/07_Car/AeroDynamicCar.cs
+++ Fabian/Week07/07_Car/AeroDynamicCar.cs
@@ -2,13 +2,6 @@
 {
     public class AeroDynamicCar : Car
     {
-        private string _Producer;
-        private string _Model;
-        private Engine _Engine;
-        private double _Weight;
-        private Tank _Tank;
-        private double _Consumption;
-        private CarDriveType _DriveType;
         public AeroDynamicCar(string producer, string model, Engine engine, double weight, Tank tank, CarDriveType carDriveType) : base(producer, model, engine, weight, tank, carDriveType)
         {
 
