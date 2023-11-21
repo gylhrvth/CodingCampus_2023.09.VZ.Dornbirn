f0843d73a843b8f7dfc4c8bc9815c74665f2a83a
Fabii137 <krsticfabian@gmail.com>
2023-11-21 11:30:18 +0100

    .

--- Fabian/Program.cs
+++ Fabian/Program.cs
@@ -1,5 +1,4 @@
 ﻿//Fabian.Week07._07Car.CarMain.Start();
 //Fabian.Week06._04Zoo.ZooMain.Start();
-Fabian.Week07._08_Bank.BankMain.Start();
-
-//Fabian.Week07.RoomExample.RoomMain.Start();
+//Fabian.Week07._08_Bank.BankMain.Start();
+Fabian.Week08._05_Museum.MuseumMain.Start();
---
+++ Fabian/Week08/05_Museum/MuseumMain.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Fabian.Week08._05_Museum
+{
+    public class MuseumMain
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
