510e38f49daa9c789f3fea6a817d45d5e8448a13
Jovo <jovo.drinic@hotmail.com>
2023-11-21 08:33:44 +0100

    new

---
+++ Jovo/Week07/Vererbung/Sportwagen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jovo.Week07.Vererbung
+{
+    internal class Sportwagen
+    {
+        protected string _Name;
+        private int _Year;
+
+
+        public Sportwagen(string name, int year) 
+        {
+            _Name = name;
+            _Year = year;
+        }
+
+
+
+        public virtual void PrintMe() 
+        {
+            Console.WriteLine(_Name);
+        }
+    }
+}
