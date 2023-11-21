a677d84f52b6e3b681f74f787dddd4ca127327e0
Gyula Horvath <gylhrvth@gmail.com>
2023-11-21 16:02:30 +0100

    Museum Example

---
+++ Gyula/Week08/MuseumSim/MuseumMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Gyula.Week08.MuseumSim
+{
+	public class MuseumMain
+	{
+		public static void Start()
+		{
+
+			Hallway mainEntry = new Hallway("Entry");
+			Museum neuePinakothek = new Museum("Neue Pinakothek", mainEntry);
+
+			ExhibitionRoom room1 = new ExhibitionRoom("Raum 1");
+			mainEntry.addNeighour(room1);
+
+			ExhibitionRoom room2 = new ExhibitionRoom("Raum 2");
+			room1.addNeighour(room2);
+
+			neuePinakothek.PrintStructure("");
+
+			Guest g = new Guest("Gustav");
+			neuePinakothek.AddGuest(g);
+	
+		}
+	}
+}
+
