59dae83f069833b07dbbc643e16293a8dc87684b
Cemo38 <cemalkosar38@gmail.com>
2023-11-21 14:39:48 +0100

    Geometry finished

---
+++ Cemal/Week08/Geometry/Geometry.cs
@@ -0,0 +1,15 @@
+﻿using Cemal.Week08.Geometry.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cemal.Week08.Geometry
+{
+    public abstract class Geometryclass
+    {
+        public abstract double getCircumference();
+        public abstract double getArea();
+    }
+}
