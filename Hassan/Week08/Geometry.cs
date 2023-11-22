61ed37f432cac8db163e6be6d0af85b5d23e8857
ZaynHassan7 <zaynhassan9@gmail.com>
2023-11-22 15:04:33 +0100

    we made the museumgit add .git add .

---
+++ Hassan/Week08/Geometry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassan.Week08
+{
+    public abstract class Geometry
+    {
+        public abstract double GetCircumference();
+        public abstract double GetArea();
+    }
+}
---
+++ Hassan/Week08/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassan.Week08
+{
+    public class Rectangle : Geometry
+    {
+        private double _lenght;
+        private double _height;
+
+
+        public Rectangle(double length, double height)
+        {
+            _lenght = length;
+            _height = height;
+        }
+
+        public override double GetCircumference()
+        {
+            return 2 * _lenght + 2 * _height;
+        }
+
+        public override double GetArea()
+        {
+            return _lenght * _height;
+        }
+
+    }
+}
