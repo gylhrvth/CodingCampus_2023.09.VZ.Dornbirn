887d6c6bc13a5abaa89dc0c0b8db56528c0bb22c
Sven Juen <juensv@gmail.com>
2023-11-23 10:07:40 +0100

    museum

--- Sven/Program.cs
+++ Sven/Program.cs
@@ -8,7 +8,8 @@
 //Sven.Week06.OOPList.List.Start();
 //Sven.Week06.SelbstTest.SelbstTest.Start();
 //Sven.Week07.Zoo.Zoo.Start();
-Sven.Week07.Modelierung.AutoHändler.Start();
+//Sven.Week07.Modelierung.AutoHändler.Start();
+Sven.Week07.Geometry.GeometryMain.Start();
 
 
   
--- Sven/Week07/Geometry/Triangle.cs
+++ Sven/Week07/Geometry/Triangle.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Sven.Week07.Geometry
 {
-    internal class Triangle
+    internal class Triangle : Geometry
     {
+        private double _length;
+        private double _height;
+
+        public Triangle(double length, double height)
+        {
+            _length = length;
+            _height = height;
+        }
+        public override double CircumFerence()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Triangle:");
+            Console.ForegroundColor = ConsoleColor.White;
+            return _length + _length + _length;
+        }
+        public override double GetArea()
+        {
+            return _length * _height / 2;
+        }
     }
 }
