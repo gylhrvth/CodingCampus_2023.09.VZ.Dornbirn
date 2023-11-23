284508135cc0de54be05b7f45e2a1c90b6bbee64
Martin <schwendingermartin2@gmail.com>
2023-11-23 13:26:48 +0100

    Files

---
+++ Martin/Week08/Files/File.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martin.Week08.Files
+{
+    public class File
+    {
+        public static void Start()
+        {
+            string dicretory = Directory.GetCurrentDirectory();
+            string[] Files = Directory.GetFiles(dicretory); //bekommt die file
+
+            Console.WriteLine("Creation Date: " + Directory.GetCreationTime(dicretory)); //erstellung datum von dicretory
+            DirectoryInfo directoryInfo = new DirectoryInfo(dicretory); //aktuellen Ordner zur obersten Ebene zu navigieren.
+
+            string orderName = directoryInfo.Parent.Parent.Parent.FullName;
+
+            Filesystemtraversal(orderName);
+
+            //PrintFolder(orderName);
+
+            //Filesystemtraversal(orderName);
+
+            //PrintFilesSizes(Files);
+            //CalculateFileSize(orderName);
+
+        }
+
+        public static void PrintFilesSizes(string[] Files)
+        {
+            foreach (string file in Files)
+            {
+                Console.WriteLine("Size: " + file.Length);
+            }
+        }
+
+        public static void PrintFolder(string folder)
+        {
+            Console.WriteLine("Currentfolder: " + folder);
+        }
+
+        public static double CalculateFileSize(string folder)
+        {
+            DirectoryInfo fileInfo = new DirectoryInfo(folder);
+
+
+            FileInfo[] Files = fileInfo.GetFiles();
+
+            double size = 0;
+
+            foreach (FileInfo file in Files)
+            {
+                size += file.Length;
+            }
+
+            Console.WriteLine("Size: {0} Mbbytes",ConvertToMbyte(size));
+
+            return size;
+
+        }
+
+        public static double ConvertToMbyte(double bytes)
+        {
+            return  bytes / 1024 / 1024;
+        }
+
+        public static void Filesystemtraversal(string ordner)
+        {
+            string dicretory = Directory.GetCurrentDirectory();
+
+            Console.WriteLine(dicretory);
+
+            foreach()
+        }
+
+
+    }
+}
---
+++ Martin/Week08/InterfaceGeometry/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martin.Week08.InterfaceGeometry
+{
+    public class Square : Rectangle
+    {
+        private double _lenght;
+
+        public Square(double lenght) : base(lenght, lenght)
+        {
+            _lenght = lenght;
+        }
+
+        public  double getCircumference()
+        {
+            return 4 * _lenght;
+        }
+
+        public  double getArea()
+        {
+            return _lenght * _lenght;
+        }
+    }
+}
