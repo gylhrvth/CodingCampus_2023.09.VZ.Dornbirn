b0b57b0498bc4db1feb0c75dc39218cca51cb601
davidisele <david.isele88@gmail.com>
2023-11-23 11:00:37 +0100

    thursday morning

---
+++ David/Week08/Filesystem/Files_DateinUOrdner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace David.Week08.Filesystem
+{
+	public class Files_DateinUOrdner
+	{
+		public Files_DateinUOrdner()
+		{
+		}
+
+		public static void Start()
+		{
+			string currentDirectory = Directory.GetCurrentDirectory();
+			Console.WriteLine(currentDirectory);
+			Console.WriteLine();
+
+			string[] children = Directory.GetFiles(currentDirectory);
+
+			var currentDirectroryInfo = new DirectoryInfo(currentDirectory);
+			string rootDirectory = currentDirectroryInfo.Parent.Parent.Parent.Parent.Parent.Parent.FullName;
+			Console.WriteLine(rootDirectory);
+
+			foreach (var directory in currentDirectory)
+			{
+				Console.Write(directory);
+			}
+
+			foreach (var child in children)
+			{
+				FileInfo fileInfo = new FileInfo(child);
+
+				Console.WriteLine(fileInfo.Name + " --> " + Math.Round(ConvertToMByte(fileInfo.Length), 2) + "KB");
+			}
+		}
+
+        public static float ConvertToMByte(long sizeInByte)
+        {
+            return sizeInByte / 1024.0f;
+        }
+    }
+}
+
+
--- David/Week08/GeometryInterface/GeometryInterfaceMain.cs
+++ David/Week08/GeometryInterface/GeometryInterfaceMain.cs
@@ -8,36 +8,36 @@ namespace David.Week08.GeometryInterface
             public static void Start()
             {
 
-                Triangle triangle = new Triangle(4);
-                Circle circle = new Circle(5);
-                Rectangle rectangle = new Rectangle(1, 2);
-                Square square = new Square(3);
+            //    Triangle triangle = new Triangle(4);
+            //    Circle circle = new Circle(5);
+            //    Rectangle rectangle = new Rectangle(1, 2);
+            //    Square square = new Square(3);
 
-                List<Geometry> geometries = new List<Geometry>()
-            {
-                triangle,
-                circle,
-                rectangle,
-                square
-            };
+            //    List<Geometry> geometries = new List<Geometry>()
+            //{
+            //    triangle,
+            //    circle,
+            //    rectangle,
+            //    square
+            //};
 
-                double sumArea = 0;
-                double sumCircumfence = 0;
+            //    double sumArea = 0;
+            //    double sumCircumfence = 0;
 
-                foreach (Geometry element in geometries)
-                {
-                    Console.WriteLine("Der Umfang beträgt = " + element.getArea() + "cm");
-                    Console.WriteLine("Die Fläche beträgt = " + element.getCircumfence() + "cm");
-                }
+            //    foreach (Geometry element in geometries)
+            //    {
+            //        Console.WriteLine("Der Umfang beträgt = " + element.getArea() + "cm");
+            //        Console.WriteLine("Die Fläche beträgt = " + element.getCircumfence() + "cm");
+            //    }
 
-                foreach (Geometry element in geometries)
-                {
-                    sumArea += element.getArea();
-                    sumCircumfence += element.getCircumfence();
-                }
+            //    foreach (Geometry element in geometries)
+            //    {
+            //        sumArea += element.getArea();
+            //        sumCircumfence += element.getCircumfence();
+            //    }
 
-                Console.WriteLine($"The sum of the Areas is : {sumArea}");
-                Console.WriteLine($"The sum of the Circumfences is : {sumCircumfence}");
+            //    Console.WriteLine($"The sum of the Areas is : {sumArea}");
+            //    Console.WriteLine($"The sum of the Circumfences is : {sumCircumfence}");
             }
         }
     }
