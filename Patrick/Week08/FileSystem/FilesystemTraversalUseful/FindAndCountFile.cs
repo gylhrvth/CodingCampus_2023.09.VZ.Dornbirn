19d2b11879c014d5baa0deacf9f50d3f5a4a4bb9
Paepm <mair.patrick@gmx.at>
2023-11-24 18:29:19 +0100

    Traversal

---
+++ Patrick/Week08/FileSystem/FilesystemTraversalUseful/FindAndCountFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrick.Week08.FileSystem.FilesystemTraversalUseful
+{
+    internal class FindAndCountFile : IFilereceiver
+    {
+        private int _Count = 0;
+
+        public int Count
+        {
+            get => _Count;
+        }
+        public void OnFileReceived(int depth, string rootFolder)
+        {
+
+            FileInfo fileInfo = new FileInfo(rootFolder);
+            string findFileName = fileInfo.FullName;
+            string countains = "VehicleMain.cs";
+            if (findFileName == countains)
+            {
+                _Count++;
+            }
+        }
+        public void OnDirectoryReceived(int depth, string rootFolder)
+        {
+
+
+        }
+
+
+
+    }
+}
