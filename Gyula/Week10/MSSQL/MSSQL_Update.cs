5a512c534684afa314c647442a33817a3a605dbb
Gyula Horvath <gylhrvth@gmail.com>
2023-11-27 13:15:53 +0100

    Merge

---
+++ Gyula/Week10/MSSQL/MSSQL_Update.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Gyula.Week10.MSSQL
+{
+	public class MSSQL_Update
+	{
+        public static void Start()
+        {
+            string connectionString = "Persist Security Info=False;Initial Catalog=Mondial;server=tcp:192.168.188.78,1433;User=gyula;Password=gyula";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("update city set Population = Population + @Amount where Name = @Name", connection);
+
+                command.Parameters.Add("@Amount", SqlDbType.Int).Value = -1;
+                command.Parameters.Add("@Name", SqlDbType.NVarChar, 80).Value = "Dornbirn";
+                try
+                {
+                    command.Connection.Open();
+                    int effectedRows = command.ExecuteNonQuery();
+
+                    Console.WriteLine($"{effectedRows} rows has been updated.");
+                }
+                catch (SqlException se)
+                {
+                    Console.WriteLine(se.Message);
+                }
+            }
+        }
+	}
+}
+
