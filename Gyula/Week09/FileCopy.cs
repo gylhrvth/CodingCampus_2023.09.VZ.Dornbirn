dd8f4c7a1fb2ea18f048caa31d82b49aec2bdf79
Gyula Horvath <gylhrvth@gmail.com>
2023-11-28 13:51:50 +0100

    StreamReader and StreamWriter

---
+++ Gyula/Week09/FileCopy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Gyula.Week09
+{
+	public class FileCopy
+	{
+		public static void Start()
+		{
+			CopyTo("", "");
+		}
+
+		public static void CopyTo(string source, string destination)
+		{
+			using (StreamReader rs = new StreamReader(source))
+			{
+				using (StreamWriter sw = new StreamWriter(destination))
+				{
+					string line = rs.ReadLine();
+
+					while (line != null)
+					{
+						sw.WriteLine(line);
+						line = rs.ReadLine();
+					}
+				}
+			}
+		}
+	}
+}
+
