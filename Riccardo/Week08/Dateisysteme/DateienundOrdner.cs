5e9bf9d10e93caeabf9bc9f299c77577dd93dbe6
Riccardo Ceccon <riccardo.ceccon@blum.com>
2023-11-28 14:00:20 +0100

    Week09

--- Riccardo/Week08/Dateisysteme/DateienundOrdner.cs
+++ Riccardo/Week08/Dateisysteme/DateienundOrdner.cs
@@ -8,5 +8,78 @@ namespace Riccardo.Week08.Dateisysteme
 {
     internal class DateienundOrdner
     {
+
+        public static void Start()
+        {
+            string currentDirecotry = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirecotry);
+
+            Console.WriteLine("Files");
+
+            string[] children = Directory.GetFiles(currentDirecotry);
+
+            foreach (var child in children)
+            {
+                FileInfo fileInfo = new FileInfo(child);
+
+                Console.WriteLine(fileInfo.Name + " ");
+            }  
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        public static void Start()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirectory);
+
+            Console.WriteLine("Files");
+
+            string[] children = Directory.GetFiles(currentDirectory);
+
+            foreach (var child in children)
+            {
+                FileInfo fileInfo = new FileInfo(child);
+
+                Console.WriteLine(fileInfo.Name + " " + Math.Round(ConvertToMByte(fileInfo.Length), 2) + "mb");
+            }
+
+            Console.WriteLine("Children Directories");
+
+
+        }
+        public static float ConvertToMByte(long sizeInByte)
+        {
+            return sizeInByte / 1024.0f / 1024;
+        }
+        */
     }
 }
