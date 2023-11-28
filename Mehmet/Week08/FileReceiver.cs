596087b1b61948c5e924eeca6264c46671e8287b
Mehmet <kalay.mehmet@outlook.com>
2023-11-28 09:00:48 +0100

    Update1

---
+++ Mehmet/Week08/FileReceiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mehmet.Week08
+{
+    public interface FileReceiver
+    {
+        public void OnFileReceived(int depth, string path);
+        public void OnDirectoryReceived(int depth, string path);
+    }
+}
