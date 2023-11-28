4ffe5480c3cafe0f0aa41c77a27d897d054d5c49
Gyula Horvath <gylhrvth@gmail.com>
2023-11-28 12:28:13 +0100

    MySql Prepareted Statement

--- Gyula/Program.cs
+++ Gyula/Program.cs
@@ -1,8 +1,10 @@
-﻿using Gyula.Week10.MSSQL;
+﻿using Gyula.Week10.MySql;
 
 //FileList.Start();
 //WriteTextFile.Start();
 //ReadTextFile.Start();
 
 //MSSQL_PreparedStatement.Start();
-MSSQL_Update.Start();
+//MSSQL_Update.Start();
+
+MySQL_PreparedStatement.Start();
