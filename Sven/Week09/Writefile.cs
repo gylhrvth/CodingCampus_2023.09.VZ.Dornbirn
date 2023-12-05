4d7b4731277c169d9b0577882588cbd7e8370c6a
Sven Juen <juensv@gmail.com>
2023-12-05 13:42:26 +0100

    SQL

---
+++ Sven/Week09/Writefile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sven.Week09
+{
+    internal class Writefile
+    {
+
+        public static void Start()
+        {
+
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\juens\\source\\repos\\CodingCampus_2023.09.VZ.Dornbirn\\Sven\\Week09\\Hello.txt"))
+            {
+
+                sw.WriteLine("Hello World!!");
+
+            }
+        }
+
+    }
+}
---
+++ Sven/Week11/MSSQL_ConnectExample.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sven.Week11
+{
+    internal class MSSQL_ConnectExample
+    {
+        public static void Start()
+        {
+
+            string connectionString = "Persist Security Info=False;Initial Catalog=Mondial;server=tcp:Local_Host;User=sven;Password=ihesfb2010";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand("SELECT country.Name, country.Population, country.Capital\r\nFROM Country\r\nWHERE Country.Name LIKE 'A%'");
+                try
+                {
+
+                    command.Connection.Open();
+
+                    Console.WriteLine("Verbindung wurde aufgebaut.");
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<DbColumn> header = reader.GetColumnSchema().ToList();
+
+                        for (int i = 0; i < header.Count; i++)
+                        {
+                            Console.Write(header[i].ColumnName + " " + header[i].DataTypeName + "[" + header[i].ColumnSize + "]    ");
+                        }
+                        Console.WriteLine();
+
+
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+
+                                Console.Write(reader[i] + " ");
+
+                            }
+                            Console.WriteLine();
+
+                        }
+
+                    }
+
+                }
+                catch (SqlException sqle)
+                {
+                    Console.WriteLine(sqle.Message);
+                }
+
+            }
+        }
+
+    }
+}
