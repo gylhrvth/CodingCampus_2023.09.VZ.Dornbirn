57b206aa59c7eff00a39009fc72b1d2764d46c6b
Fabienne <fabienne.bischof22@gmail.com>
2023-12-05 10:47:21 +0100

    sql

---
+++ Fabienne/Week10/SQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabienne.Week10
+{
+    public class SQL
+    {
+        public static void Start()
+        {
+            string ConnectionString = "server=localhost;port=3306;user=Fabienne;password=fabienne;database:mondial";
+
+            
+
+        }
+    }
+}
