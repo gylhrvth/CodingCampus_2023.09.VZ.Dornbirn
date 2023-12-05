d17838014ab31dbcfd7e9a877bf4fce8f8eb5d37
Dario Andrade <daae@bachmann.at>
2023-12-05 10:43:42 +0100

    safddsa

---
+++ DarioLuis/Week08/Datein_und_Ordner.cs
@@ -0,0 +1,135 @@
+﻿namespace DarioLuis.Week08
+{
+    public class Datein_und_Ordner
+    {
+        public static void Start()
+        {
+            Ordner();
+            Aufgab2();
+        }
+        public static void Ordner()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string[] children = Directory.GetFiles(currentDirectory);
+
+            var currentDirectoryInfo = new DirectoryInfo(currentDirectory);
+            string projectRootDirectory = currentDirectoryInfo.Parent.Parent.Parent.Parent.FullName;
+
+
+            Console.Write("Project Digital Campus Directory: ");
+
+            //Path
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(projectRootDirectory);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string[] directoryChildren = Directory.GetDirectories(projectRootDirectory);
+            string[] directoryChildrenFiles = Directory.GetFiles(projectRootDirectory);
+
+            foreach (var directory in directoryChildren)
+            {
+                FileInfo fileInfo = new FileInfo(directory);
+                DirectoryInfo info = new DirectoryInfo(directory);
+                if (Directory.Exists(info.FullName))
+                {
+                    Console.WriteLine(info.Name);
+                }
+            }
+
+            //Ausgabe
+            foreach (var directory in directoryChildrenFiles)
+            {
+                FileInfo fileInfo = new FileInfo(directory);
+                DirectoryInfo info = new DirectoryInfo(directory);
+                if (File.Exists(fileInfo.FullName))
+                {
+                    Console.Write(fileInfo.Name + " hat insgesamt ");
+                    if (Math.Round(ConvertToKB(fileInfo.Length)) >= 10)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(Math.Round(ConvertToKB(fileInfo.Length)));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("KB");
+
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(Math.Round(ConvertToKB(fileInfo.Length)));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("KB");
+                    }
+                }
+            }
+        }
+        public static float ConvertToKB(long sizeInByte)
+        {
+            return sizeInByte / 1024.0f;
+        }
+
+        public static void Aufgab2() 
+        {
+            // Den Weg definieren und eine Variable zuweisen
+            string directoryPath = @"C:\Users\daae\source\repos\CodingCampus_2023.09.VZ.Dornbirn";
+
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+
+            //Ausgabe
+            if (directoryInfo.Exists)
+            {
+
+                long directorySize = GetDirectorySize(directoryInfo);
+
+
+                Console.Write($"Größe vom Ordner: '{directoryPath}': ");
+                if (directorySize >= 30)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{FormatBytes(directorySize)}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+
+        //Heraus finden wie groß der Ordner ist
+        static long GetDirectorySize(DirectoryInfo directory)
+        {
+            long size = 0;
+
+
+            foreach (var file in directory.GetFiles())
+            {
+                size += file.Length;
+            }
+
+
+            foreach (var subdirectory in directory.GetDirectories())
+            {
+                size += GetDirectorySize(subdirectory);
+            }
+
+            return size;
+        }
+
+        //Welche Einheit es haben soll
+
+        static string FormatBytes(long bytes)
+        {
+            string[] sizeSuffixes = { "B", "KB", "MB", "GB", "TB" };
+            int i = 0;
+
+            while (bytes >= 1024 && i < sizeSuffixes.Length - 1)
+            {
+                bytes /= 1024;
+                i++;
+            }
+
+            return $"{bytes} {sizeSuffixes[i]}";
+        }
+        
+    }
+}
+
+
---
+++ DarioLuis/Week08/Gewicht_Umrechner.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lukas.Week08
+{
+    public class Gewicht_Umrechner
+    {
+        public static void Start()
+        {
+            First();
+            Call_Calculate();
+        }
+
+        public static void First()
+        {
+            Console.Write("Hallo das ist der Gewichtrechner von Dario.");
+            Console.WriteLine("(Dario Andrade, 2023, © Copyright for all Rights for Dario Andrade)");
+        }
+        public static void Call_Calculate()
+        {
+            Console.WriteLine("Hallo welches Gewicht hast du gerade?(g, kg)");
+            string entry = Console.ReadLine().ToLower();
+
+            Console.WriteLine("In welches Gewicht magst du Umrechen?(µg, mg, g, kg, t, gt)");
+            string subentry = Console.ReadLine().ToLower();
+
+            //kg
+            if(entry == "kg")
+            {
+                Console.WriteLine("Wie viel Kg magst du umrechnen?");
+                int numb = Convert.ToInt32(Console.ReadLine());
+
+                //kg in mikrogramm
+                if (subentry == "µg")
+                {
+                    double endkgµg = numb / 0.0000000010000;
+                    Console.Write(numb + " in Mikrogramm: ");
+                    Console.Write(endkgµg);
+                    Console.WriteLine("µg");
+                }
+
+                //kg in milligramm
+                if (subentry == "mg")
+                {
+                    double endkgmg = numb * 1000 * 1000;
+                    Console.Write(numb + " in Milligramm: ");
+                    Console.WriteLine(endkgmg + "mg");
+                }
+
+                //kg in gramm
+                if (subentry == "g")
+                {
+                    double endkgg = numb * 1000;
+                    Console.Write(numb + " in Gramm: ");
+                    Console.WriteLine(endkgg + "g");
+                }
+
+                //kg in kg
+                if (subentry == "kg")
+                {
+                    Console.WriteLine("Bist nicht gerade der schlauste oder? " + numb + " = " + numb);
+
+                }
+
+                //kg in tonne
+                if (subentry == "t")
+                {
+                    double endkgt = numb / 1000;
+                    Console.Write(numb + " als Tonne: ");
+                    Console.WriteLine(endkgt + "t");
+                }
+                
+                //kg in gigatonne
+                if (subentry == "gt")
+                {
+                    double endkggt = numb / 1000 / 1000;
+                    Console.Write( numb + " als Gigatonne: ");
+                    Console.WriteLine(endkggt + "gt");
+                }
+            }
+
+            //=======================================
+            //gramm
+
+            //g
+            if (entry == "g")
+            {
+                Console.WriteLine("Wie viel g magst du umrechnen?");
+                int numb = Convert.ToInt32(Console.ReadLine());
+
+                //g in mikrogramm
+                if (subentry == "µg")
+                {
+                    double endkgµg = numb * 1000000;
+                    Console.Write(numb + " in Mikrogramm: ");
+                    Console.Write(endkgµg);
+                    Console.WriteLine("µg");
+                }
+
+                //g in milligramm
+                if (subentry == "mg")
+                {
+                    double endkgmg = numb * 1000;
+                    Console.Write(numb + " in Milligramm: ");
+                    Console.WriteLine(endkgmg + "mg");
+                }
+
+                //g in gramm
+                if (subentry == "kg")
+                {
+                    double endkgg = numb / 1000;
+                    Console.Write(numb + " in Gramm: ");
+                    Console.WriteLine(endkgg + "kg");
+                }
+
+                //g in kg
+                if (subentry == "g")
+                {
+                    Console.WriteLine("Bist nicht gerade der schlauste oder? " + numb + " = " + numb);
+
+                }
+
+                //g in tonne
+                if (subentry == "t")
+                {
+                    double endkgt = numb / 1000 / 1000;
+                    Console.Write(numb + " als Tonne: ");
+                    Console.WriteLine(endkgt + "t");
+                }
+
+                //g in gigatonne
+                if (subentry == "gt")
+                {
+                    long  endkggt = numb / 1000 / 1000 / 1000;
+                    Console.Write(numb + " als Gigatonne: ");
+                    Console.WriteLine(endkggt + "gt");
+                }
+            }
+        }
+    }
+}
