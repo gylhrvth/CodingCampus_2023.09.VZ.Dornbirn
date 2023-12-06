81599ae62f37af647dc99a7c2e4afc2ff7480c3b
Riccardo Ceccon <riccardo.ceccon@blum.com>
2023-12-06 13:37:37 +0100

    MSSQL_Userabfrage

--- Riccardo/Week10/MSSQL_ConnectExample.cs
+++ Riccardo/Week10/MSSQL_ConnectExample.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 namespace Riccardo.Week10
 {
     internal class MSSQL_ConnectExample
-    {
+    {/*
         public static void Start() { 
         string connectionString = "Persist Security Info=False;Initial Catalog=Mondial;server=tcp;192.168.0.197,1433;User=riccrado;"
 
         }
+    }*/
+
     }
 }
