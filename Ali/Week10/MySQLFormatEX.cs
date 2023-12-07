7ca061762a64d2db6d53af3a3931dd8ec9ede6f0
AliAsuk <asuk_ali@icloud.com>
2023-12-07 13:14:43 +0100

    MYSQL

---
+++ Ali/Week10/MySQLFormatEX.cs
@@ -0,0 +1,122 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Ali.Week10
+{
+    public class MySQLFormatEX
+    {
+        public static void Start()
+        {
+            Console.Write("Welches Land möchtest du sehen? ");
+            string countryName = Console.ReadLine();
+
+            string connectionString = "server=localhost;port=3306;user=Ali;password=Denizli20@;database=Mondial";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.ForegroundColor = ConsoleColor.Green; 
+                    Console.WriteLine("Verbindung wurde erfolgreich hergestellt.");
+                    Console.ResetColor();
+
+                    MySqlCommand command = new MySqlCommand("SELECT c.* FROM City c JOIN country co ON c.Country = co.Code WHERE co.Name = @CountryName", connection);
+                    command.Parameters.AddWithValue("@CountryName", countryName);
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    List<DbColumn> header = reader.GetColumnSchema().ToList();
+
+                    
+                    PrintTableRow(header, ConsoleColor.Blue);
+
+                    while (reader.Read())
+                    {
+                        
+                        PrintTableRow(reader, header, ConsoleColor.White);
+                    }
+
+                    reader.Close();
+                }
+                catch (MySqlException sqlE)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                    Console.WriteLine("Fehler bei der MySQL-Verbindung: " + sqlE.Message);
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        private static void PrintTableRow(IEnumerable<DbColumn> columns, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            foreach (var column in columns)
+            {
+                string formatString = GetFormatString(column);
+                Console.Write(formatString, column.ColumnName);
+            }
+            Console.WriteLine("|");
+            Console.ResetColor();
+        }
+
+        private static void PrintTableRow(MySqlDataReader reader, IEnumerable<DbColumn> columns, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write("| ");
+                string formatString = GetFormatString(columns, i);
+                Console.Write(formatString, reader[i]);
+            }
+            Console.WriteLine("|");
+            Console.ResetColor();
+        }
+
+        private static string GetFormatString(DbColumn column)
+        {
+            int columnSize = Math.Max((int)column.ColumnSize, (int)column.ColumnName.Length);
+
+            if (column.DataType == typeof(string))
+            {
+                return $"| {{0,{-columnSize}}} ";
+            }
+            else if (column.DataType == typeof(float))
+            {
+                return $"{{0,{columnSize}}} ";
+            }
+            else
+            {
+                return $"{{0,{columnSize}}} ";
+            }
+        }
+
+        private static string GetFormatString(IEnumerable<DbColumn> columns, int index)
+        {
+            var column = columns.ElementAt(index);
+            int columnSize = Math.Max((int)column.ColumnSize, (int)column.ColumnName.Length);
+
+            if (column.DataType == typeof(string))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                return $"{{0,{-columnSize}}} ";
+            }
+            else if (column.DataType == typeof(float))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                return $"{{0,{columnSize}:N2}} ";
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                return $"{{0,{columnSize}}} ";
+            }
+        }
+    }
+}
