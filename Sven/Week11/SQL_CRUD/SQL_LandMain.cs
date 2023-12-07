d071ed329e89c9001431c51e91380882acb87ac2
Sven Juen <juensv@gmail.com>
2023-12-07 15:48:31 +0100

    ok

--- Sven/Week11/SQL_CRUD/SQL_LandMain.cs
+++ Sven/Week11/SQL_CRUD/SQL_LandMain.cs
@@ -8,5 +8,21 @@ namespace Sven.Week11.SQL_CRUD
 {
     internal class SQL_LandMain
     {
+        public static void Start()
+        {
+            List<Land> datenbank = new List<Land>();
+
+            Land.LandHinzufügen(datenbank, "Alabasta", "Grandline" , 25528708);
+
+            Land gefundenesLand = Land.LandFinden(datenbank, "Alabasta");
+            Console.WriteLine($"Gefundenes Land: {gefundenesLand.Name}, Hauptstadt: {gefundenesLand.Hauptstadt}, Bevölkerung: {gefundenesLand.Bevölkerung}");
+
+            gefundenesLand.AttributVerändern("Hauptstadt", "Alburna");
+            Console.WriteLine($"Geänderte Hauptstadt: {gefundenesLand.Hauptstadt}");
+
+            Land.LandLöschen(datenbank, "Alabasta");
+
+        }
     }
 }
+
