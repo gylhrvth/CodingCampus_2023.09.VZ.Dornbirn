3c63cc2c64d035c5be7068790b3f7c604ee1da84
davidisele <david.isele88@gmail.com>
2023-12-13 09:22:13 +0100

    wednesday

--- David/Week11/uebung/CountryStart.cs
+++ David/Week11/uebung/CountryStart.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using MySql.Data.MySqlClient;
+
 namespace David.Week11.uebung
 {
 	public class CountryStart
@@ -6,6 +11,12 @@ namespace David.Week11.uebung
 		public CountryStart()
 		{
 		}
+
+		public static void Start()
+		{
+			string connectionString = "server=localhost;port=3306;user=david;pwd=david;database=mondial";
+			Database 
+        }
 	}
 }
 
--- David/Week11/uebung/database.cs
+++ David/Week11/uebung/database.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using MySql.Data.MySqlClient;
+
 namespace David.Week11.uebung
 {
 	public class database
 	{
-		public database()
+		private string connectionString;
+		MySqlConnection connection;
+
+		public database(string connectionString)
 		{
+			this.connectionString = connectionString;
 		}
 	}
 }
