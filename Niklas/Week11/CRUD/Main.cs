7da096892428574e5a6729e44daf4118bb41c969
NILA <NILA@ATFKNBNILA01>
2023-12-13 09:24:32 +0100

    test

---
+++ Niklas/Week11/CRUD/Main.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niklas.Week11.CRUD
+{
+    internal class Main
+    {
+        public static void Start()
+        {
+            string connectionString = "port=3306;user=Niklas;password=6C#fw,_.UDbLa&g;database=Mondial;server=localhost";
+            
+            
+        }
+    }
+}
