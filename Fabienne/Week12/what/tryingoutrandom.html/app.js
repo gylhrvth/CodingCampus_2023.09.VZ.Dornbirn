91ef73c87c1566df816b40a4317892145145e1f6
Fabienne <fabienne.bischof22@gmail.com>
2023-12-20 09:37:21 +0100

    hi

---
+++ Fabienne/Week12/what/tryingoutrandom.html/app.js
@@ -0,0 +1,11 @@
+function togglePlayPause() {
+    // Implement play/pause functionality
+  }
+  
+  function previousTrack() {
+    // Implement previous track functionality
+  }
+  
+  function nextTrack() {
+    // Implement next track functionality
+  }
