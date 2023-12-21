db9ad64413e7dee196718ad2d69a8755fcb6c79a
Cemo38 <cemalkosar38@gmail.com>
2023-12-21 15:56:56 +0100

    Website

--- Cemal/Week12/javascript.js
+++ Cemal/Week12/javascript.js
@@ -8,16 +8,3 @@ document.getElementById("showImage").onclick = function () {
         x.style.display = "none";
     }
 }
-
-document.getElementById("showsexy").onclick = function () {
-    var x = document.getElementById("sexy");
-    console.log(">>>" + x.style.display + "<<<")
-
-    if (!x.style.display || x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-
