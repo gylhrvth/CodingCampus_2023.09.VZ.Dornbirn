b0c2a82da40137685a0effaaf8cab9254a572eb5
Mohammed <alsarajimohammed@gmail.com>
2023-12-22 08:59:36 +0100

    Update

---
+++ Mohammed/vuejs/first project/src/stores/counter.ts
@@ -0,0 +1,15 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', {
+  state: () => {
+    return { count: 0 }
+  },
+  // could also be defined as
+  // state: () => ({ count: 0 })
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+})
---
+++ Mohammed/vuejs/first project/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>Counter clicked {{ counterStore.count }}</h2>
+  </div>
+</template>
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+
+const counterStore = useCounterStore()
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
