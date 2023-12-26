986d41439d48eebd8202d495bee1c73bfa7e720d
davidisele <david.isele88@gmail.com>
2023-12-26 11:12:35 +0100

    x-mas holiday

---
+++ David/Week13/hello-world/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let age = 20;
+if (age < 50) {
+    age += 10;
+}
+const result = summe(5, 4);
+function summe(a, b) {
+    return a + b;
+}
+//# sourceMappingURL=index.js.map
---
+++ David/Week13/hello-world/src/index.ts
@@ -0,0 +1,10 @@
+let age: number = 20;
+if (age < 50){
+    age += 10;
+}
+
+const result = summe(5, 4);
+
+function summe(a: number, b: number) {
+    return a + b;
+}
