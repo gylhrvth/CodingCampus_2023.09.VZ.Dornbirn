237b543badf13d1615f925b1932c49186750d808
Dimitri <github@dmtrh.eu>
2024-01-03 15:56:38 +0100

    refactor some functions try out remove

--- Dimitri/Week13/ToDo/dist/main.js
+++ Dimitri/Week13/ToDo/dist/main.js
@@ -1,28 +1,11 @@
 "use strict";
-function getTextFromField() {
-    let input = document.getElementById("toDoInput").value;
-    console.log(input);
-    console.log(document.getElementById("emptyStringWarning") == null);
+function addElementToDocument() {
+    let input = getTextFromInput("toDoInput");
     if ((input == "") && (document.getElementById("emptyStringWarning") == null)) {
-        let formContainer = document.getElementById("inputForm");
-        let form = document.createElement("div");
-        form.setAttribute("id", "emptyStringWarning");
-        let warningText = document.createTextNode("Please provide an input!");
-        form.appendChild(warningText);
-        formContainer === null || formContainer === void 0 ? void 0 : formContainer.appendChild(form);
+        addErrorMessage();
     }
     else if (input != "" && input != null) {
-        let container = document.getElementById("container");
-        let div = document.createElement("div");
-        let ToDoContainer = document.createElement("span");
-        div.appendChild(ToDoContainer);
-        let text = document.createTextNode(input);
-        ToDoContainer.appendChild(text);
-        let doneButton = createButton("done");
-        div.appendChild(doneButton);
-        let deleteButton = createButton("delete");
-        div.appendChild(deleteButton);
-        container === null || container === void 0 ? void 0 : container.appendChild(div);
+        addToDoItem(input);
     }
 }
 function createButton(name) {
@@ -30,5 +13,36 @@ function createButton(name) {
     inputElement.setAttribute("type", "button");
     inputElement.setAttribute("value", name);
     return inputElement;
+}
+function getTextFromInput(elementId) {
+    let inputElement = document.getElementById(elementId);
+    let input = inputElement.value;
+    return input;
+}
+function addErrorMessage() {
+    let formContainer = document.getElementById("inputForm");
+    let form = document.createElement("div");
+    form.setAttribute("id", "emptyStringWarning");
+    let warningText = document.createTextNode("Please provide an input!");
+    form.appendChild(warningText);
+    formContainer === null || formContainer === void 0 ? void 0 : formContainer.appendChild(form);
+}
+function addToDoItem(input) {
+    let container = document.getElementById("container");
+    let div = document.createElement("div");
+    let ToDoContainer = document.createElement("span");
+    div.appendChild(ToDoContainer);
+    let text = document.createTextNode(input);
+    ToDoContainer.appendChild(text);
+    let doneButton = createButton("done");
+    div.appendChild(doneButton);
+    let deleteButton = createButton("delete");
+    div.appendChild(deleteButton);
+    container === null || container === void 0 ? void 0 : container.appendChild(div);
+}
+function removeToDo() {
+    var _a;
+    let toDoItem = document.getElementById("toDoItem");
+    (_a = toDoItem.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(toDoItem);
 }
 //# sourceMappingURL=main.js.map
--- Dimitri/Week13/ToDo/src/main.ts
+++ Dimitri/Week13/ToDo/src/main.ts
@@ -1,47 +1,57 @@
 /* fun */
 
-function getTextFromField(): void {
-    let input: string = (<HTMLInputElement>document.getElementById("toDoInput")).value
+function addElementToDocument() {
 
-    // input = document.getElementById("toDoInput") as HTMLInputElement | null
-
-    // let inputValue = input?.value
-
-    console.log(input)
-
-    console.log(document.getElementById("emptyStringWarning") == null)
+    let input: string = getTextFromInput("toDoInput");
 
     if ((input == "") && (document.getElementById("emptyStringWarning") == null)) {
-        let formContainer: HTMLElement | null = document.getElementById("inputForm")
-        let form: HTMLDivElement = document.createElement("div")
-        form.setAttribute("id", "emptyStringWarning")
-        let warningText: Text = document.createTextNode("Please provide an input!")
-        form.appendChild(warningText)
-        formContainer?.appendChild(form)
+        addErrorMessage();
     } else if (input != "" && input != null) {
-        let container: HTMLElement | null = document.getElementById("container")
-        let div: HTMLDivElement = document.createElement("div")
-        let ToDoContainer: HTMLSpanElement = document.createElement("span")
-        div.appendChild(ToDoContainer)
-        let text: Text = document.createTextNode(input)
-        ToDoContainer.appendChild(text)
-        let doneButton: HTMLInputElement = createButton("done")
-        div.appendChild(doneButton)
-        let deleteButton: HTMLInputElement = createButton("delete")
-        div.appendChild(deleteButton)
-        container?.appendChild(div)
+        addToDoItem(input);
     }
 }
 
 function createButton(name: string): HTMLInputElement {
-
-
     let inputElement: HTMLInputElement = document.createElement("input")
 
     inputElement.setAttribute("type", "button")
     inputElement.setAttribute("value", name)
 
     return inputElement
+}
+
+function getTextFromInput(elementId: string): string {
+    let inputElement: HTMLInputElement = document.getElementById(elementId) as HTMLInputElement;
+    let input: string = inputElement.value;
+    return input;
+}
+
+function addErrorMessage() {
+    let formContainer: HTMLElement | null = document.getElementById("inputForm")
+    let form: HTMLDivElement = document.createElement("div")
+    form.setAttribute("id", "emptyStringWarning")
+    let warningText: Text = document.createTextNode("Please provide an input!")
+    form.appendChild(warningText)
+    formContainer?.appendChild(form)
+}
+
+function addToDoItem(input: string) {
+    let container: HTMLElement | null = document.getElementById("container")
+    let div: HTMLDivElement = document.createElement("div")
+    let ToDoContainer: HTMLSpanElement = document.createElement("span")
+    div.appendChild(ToDoContainer)
+    let text: Text = document.createTextNode(input);
+    ToDoContainer.appendChild(text);
+    let doneButton: HTMLInputElement = createButton("done");
+    div.appendChild(doneButton);
+    let deleteButton: HTMLInputElement = createButton("delete");
+    div.appendChild(deleteButton);
+    container?.appendChild(div);
+}
+
+function removeToDo() {
+    let toDoItem: HTMLDivElement = document.getElementById("toDoItem") as HTMLDivElement;
+    toDoItem.parentNode?.removeChild(toDoItem)
 }
 
 
