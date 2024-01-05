ced42c23a5b456a628468a5540177b4f8849261e
Dimitri <github@dmtrh.eu>
2024-01-05 15:01:57 +0100

    almost fully functional todo app, currently done is buggy on repeating use

--- Dimitri/Week13/ToDo/dist/main.js
+++ Dimitri/Week13/ToDo/dist/main.js
@@ -37,24 +37,69 @@ function removeErrorMessage() {
     let errorMessageDiv = document.getElementById("emptyStringWarning");
     (_a = errorMessageDiv === null || errorMessageDiv === void 0 ? void 0 : errorMessageDiv.parentElement) === null || _a === void 0 ? void 0 : _a.removeChild(errorMessageDiv);
 }
-function addToDoItem(input) {
-    let container = document.getElementById("container");
-    let div = document.createElement("div");
-    let ToDoContainer = document.createElement("span");
-    div.appendChild(ToDoContainer);
-    let text = document.createTextNode(input);
-    ToDoContainer.appendChild(text);
+function addToDoItem(userInput) {
+    let container = document.getElementById("toDoContainer");
+    let listItemElement = document.createElement("li");
+    let listItem = document.createElement("input");
+    listItemElement.appendChild(listItem);
+    listItem.setAttribute("value", userInput);
+    listItem.setAttribute("readonly", "true");
     let doneButton = createButton("done");
-    div.appendChild(doneButton);
+    listItemElement.appendChild(doneButton);
     let deleteButton = createButton("delete");
-    div.appendChild(deleteButton);
-    container === null || container === void 0 ? void 0 : container.appendChild(div);
-    div.setAttribute("class", "toDoItem");
+    listItemElement.appendChild(deleteButton);
+    let editButton = createButton("edit");
+    listItemElement.appendChild(editButton);
+    container === null || container === void 0 ? void 0 : container.appendChild(listItemElement);
+    listItemElement.setAttribute("class", "toDoItem");
     deleteButton.addEventListener("click", () => {
-        container === null || container === void 0 ? void 0 : container.removeChild(div);
+        container === null || container === void 0 ? void 0 : container.removeChild(listItemElement);
     });
     doneButton.addEventListener("click", () => {
-        ToDoContainer.style.textDecoration = "line-through";
+        listItem.style.textDecoration = "line-through";
+        doneButton.removeAttribute('value');
+        doneButton.setAttribute('value', 'undone');
+        doneButton.addEventListener("click", () => {
+            listItem.style.textDecoration = 'revert';
+            doneButton.removeAttribute('value');
+            doneButton.setAttribute('value', 'done');
+        });
     });
+    editButton.addEventListener("click", () => {
+        makeButtonVerySmall(deleteButton);
+        makeButtonVerySmall(editButton);
+        makeButtonVerySmall(doneButton);
+        let confirmEditButton = createButton("confirm");
+        listItemElement.appendChild(confirmEditButton);
+        confirmEditButton.setAttribute("id", "newEditField");
+        listItem.removeAttribute('readonly');
+        const end = listItem.value.length;
+        listItem.setSelectionRange(end, end);
+        listItem.focus();
+        confirmEditButton.addEventListener("click", () => {
+            let newText = listItem.value;
+            listItem.removeAttribute('value');
+            listItem.setAttribute("value", newText);
+            listItem.setAttribute('readonly', 'true');
+            makeButtonNormal(deleteButton);
+            makeButtonNormal(editButton);
+            makeButtonNormal(doneButton);
+            listItemElement.removeChild(confirmEditButton);
+        });
+        listItem.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                confirmEditButton.click();
+            }
+        });
+    });
+}
+function makeButtonVerySmall(button) {
+    button.style.display = 'none';
+    button.setAttribute('speak', 'none');
+}
+function makeButtonNormal(button) {
+    button.style.display = 'revert';
+    button.removeAttribute('speak');
 }
 //# sourceMappingURL=main.js.map
--- Dimitri/Week13/ToDo/src/main.ts
+++ Dimitri/Week13/ToDo/src/main.ts
@@ -48,29 +48,88 @@ function removeErrorMessage() {
     errorMessageDiv?.parentElement?.removeChild(errorMessageDiv);
 }
 
-function addToDoItem(input: string) {
-    let container = document.getElementById("container");
-    let div = document.createElement("div");
-    let ToDoContainer: HTMLSpanElement = document.createElement("span");
-    div.appendChild(ToDoContainer);
-    let text: Text = document.createTextNode(input);
-    ToDoContainer.appendChild(text);
+function addToDoItem(userInput: string) {
+    let container = document.getElementById("toDoContainer");
+    let listItemElement = document.createElement("li");
+    let listItem: HTMLInputElement = document.createElement("input");
+    listItemElement.appendChild(listItem);
+    listItem.setAttribute("value", userInput)
+    listItem.setAttribute("readonly", "true")
     let doneButton: HTMLInputElement = createButton("done");
-    div.appendChild(doneButton);
+    listItemElement.appendChild(doneButton);
     let deleteButton: HTMLInputElement = createButton("delete");
-    div.appendChild(deleteButton);
-    // todo edit button
-    // let editButton = createButton("edit")
-    // div.appendChild(editButton)
-    container?.appendChild(div);
-    div.setAttribute("class", "toDoItem");
+    listItemElement.appendChild(deleteButton);
+    let editButton = createButton("edit")
+    listItemElement.appendChild(editButton)
+    container?.appendChild(listItemElement);
+    listItemElement.setAttribute("class", "toDoItem");
+
     deleteButton.addEventListener("click", () => {
-        container?.removeChild(div);
-    })
-    doneButton.addEventListener("click", () => {
-        ToDoContainer.style.textDecoration = "line-through";
+        container?.removeChild(listItemElement);
     })
 
+    doneButton.addEventListener("click", () => {
+        listItem.style.textDecoration = "line-through";
+
+        // let undoneButton = createButton('undone');
+        // doneButton.replaceWith(undoneButton);
+        doneButton.removeAttribute('value');
+        doneButton.setAttribute('value', 'undone');
+        doneButton.addEventListener("click", () => {
+            listItem.style.textDecoration = 'revert';
+            doneButton.removeAttribute('value');
+            doneButton.setAttribute('value', 'done');
+        })
+    })
+
+    editButton.addEventListener("click", () => {
+        makeButtonVerySmall(deleteButton);
+        makeButtonVerySmall(editButton);
+        makeButtonVerySmall(doneButton);
+        let confirmEditButton = createButton("confirm");
+        listItemElement.appendChild(confirmEditButton);
+        confirmEditButton.setAttribute("id", "newEditField");
+        listItem.removeAttribute('readonly');
+
+        const end = listItem.value.length;
+        listItem.setSelectionRange(end, end);
+        listItem.focus();
+
+        confirmEditButton.addEventListener("click", () => {
+            let newText = listItem.value;
+            listItem.removeAttribute('value');
+            listItem.setAttribute("value", newText);
+            listItem.setAttribute('readonly', 'true');
+            makeButtonNormal(deleteButton);
+            makeButtonNormal(editButton);
+            makeButtonNormal(doneButton);
+            listItemElement.removeChild(confirmEditButton);
+
+        })
+
+        listItem.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                confirmEditButton.click();
+
+            }
+        })
+
+
+
+
+
+    })
+}
+
+function makeButtonVerySmall(button: HTMLInputElement) {
+    button.style.display = 'none';
+    button.setAttribute('speak', 'none');
+}
+
+function makeButtonNormal(button: HTMLInputElement) {
+    button.style.display = 'revert';
+    button.removeAttribute('speak');
 }
 
 
