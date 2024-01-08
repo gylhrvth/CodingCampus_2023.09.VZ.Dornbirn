45638237a7e6c3028241701ec6754e63cb3d6d05
Gyula Horvath <gylhrvth@gmail.com>
2024-01-08 21:09:57 +0100

    App2 2nd try

---
+++ Gyula/Projects/app2/pages/city/+Page.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>City</h1>
+    <p v-for="item in data.results">Name: {{item.Name}} in {{item.Province}}</p>
+</template>
+
+<script>
+import { usePageContext } from '../../renderer/usePageContext'
+
+const pageProps = ['data']
+export default { 
+    props: pageProps,
+
+    setup(props){
+        console.log("setup()")
+        const pageContext = usePageContext()
+        const data = pageContext.data
+    
+        console.log("PS --> ", pageContext.data)
+
+        return {data}
+    }
+}
+</script>
