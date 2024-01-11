57ea17cc3bb6e0f44d07143886703d6e2e834b73
Lukas Aichbauer <lukas.aichbauer@wolfvision.net>
2024-01-11 16:02:27 +0100

    - Loading Properties in C#.

--- Lukas/Lukas/Program.cs
+++ Lukas/Lukas/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Lukas.Week11.DatabaseStart.Start();
+using Lukas.Week11;
+
+var data = LoadProperties.Load("Lukas.configuration.properties");
+
+if (!data.ContainsKey("DATA_BASE_CONNECTION_STRING"))
+{
+    Console.WriteLine("Please specify: DATA_BASE_CONNECTION_STRING=xyz in configuration.properties");
+}
+else
+{
+    Console.WriteLine(data["DATA_BASE_CONNECTION_STRING"]);
+}
 
 Console.ReadLine();
