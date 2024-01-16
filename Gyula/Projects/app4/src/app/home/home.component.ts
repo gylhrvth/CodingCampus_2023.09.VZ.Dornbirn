24d7c24d0a633996048529d64c0184eb3bf0d3c3
Gyula Horvath <gylhrvth@gmail.com>
2024-01-16 11:03:11 +0100

    add component to home

--- Gyula/Projects/app4/src/app/home/home.component.ts
+++ Gyula/Projects/app4/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RandomUserComponent } from '../random-user/random-user.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RandomUserComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
